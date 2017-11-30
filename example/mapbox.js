9bbb2a6ed1aec8977bc79a2db3f0da11cf5c5645
David Manzanares <dmanzanares@cartodb.com>
2017-11-30 13:02:48 +0100

    revert mapbox.js

--- example/mapbox.js
+++ example/mapbox.js
@@ -1,85 +1,220 @@
-console.time('test');
-function getFBstatus(gl) {
-    const status = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
-    switch (status) {
-        case gl.FRAMEBUFFER_COMPLETE:
-            return 'FRAMEBUFFER_COMPLETE';
-        case gl.FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
-            return 'FRAMEBUFFER_INCOMPLETE_ATTACHMENT';
-        case gl.FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
-            return 'FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT';
-        case gl.FRAMEBUFFER_INCOMPLETE_DIMENSIONS:
-            return 'FRAMEBUFFER_INCOMPLETE_DIMENSIONS';
-        case gl.FRAMEBUFFER_UNSUPPORTED:
-            return 'FRAMEBUFFER_UNSUPPORTED';
-        case gl.FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
-            return 'FRAMEBUFFER_INCOMPLETE_MULTISAMPLE';
-        case gl.RENDERBUFFER_SAMPLES:
-            return 'RENDERBUFFER_SAMPLES';
-        default:
-            return 'Unkown Framebuffer status';
+import * as R from '../src/index';
+
+var VectorTile = require('@mapbox/vector-tile').VectorTile;
+var Protobuf = require('pbf');
+
+var renderer;
+var oldtiles = [];
+var ajax;
+
+var style;
+var schema;
+
+
+function styleWidth(e) {
+    const v = document.getElementById("widthStyleEntry").value;
+    try {
+        style.getWidth().blendTo(R.Style.parseStyleExpression(v, schema), 1000);
+        document.getElementById("feedback").value = 'ok';
+    } catch (error) {
+        const err = `Invalid width expression: ${error}:${error.stack}`;
+        console.warn(err);
+        document.getElementById("feedback").value = err;
     }
 }
-function getWebGLsupport(canvas) {
-    var support = {
-      webgl1: '?',
-      OES_texture_float: '?',
-      EXT_blend_minmax: '?',
-      MAX_RENDERBUFFER_SIZE: '?',
-      renderToFloat: '?'
+function styleColor(e) {
+    const v = document.getElementById("colorStyleEntry").value;
+    try {
+        style.getColor().blendTo(R.Style.parseStyleExpression(v, schema), 1000);
+        document.getElementById("feedback").value = 'ok';
+    } catch (error) {
+        const err = `Invalid color expression: ${error}:${error.stack}`;
+        console.warn(err);
+        document.getElementById("feedback").value = err;
+    }
+}
+
+function getTileList(c, iz, aspect) {
+    var list = [];
+    var z = Math.ceil(Math.log2(1. / iz));
+    var x = c.x;
+    var y = c.y;
+    const numTiles = Math.pow(2, z);
+    function saturate(x) {
+        return Math.min(Math.max(x, 0), 1);
+    }
+    const minx = Math.floor(numTiles * saturate((x - iz * aspect) * 0.5 + 0.5));
+    const maxx = Math.ceil(numTiles * saturate((x + iz * aspect) * 0.5 + 0.5));
+    const miny = Math.floor(numTiles * saturate(1. - ((y + iz) * 0.5 + 0.5)));
+    const maxy = Math.ceil(numTiles * saturate(1. - ((y - iz) * 0.5 + 0.5)));
+    for (let i = minx; i < maxx; i++) {
+        for (let j = miny; j < maxy; j++) {
+            list.push({
+                x: i,
+                y: j,
+                z: z
+            });
+        }
+    }
+    return list;
+}
+function getData(aspect) {
+    const tiles = getTileList(renderer.getCenter(), renderer.getZoom(), aspect);
+    var completedTiles = [];
+    var needToComplete = tiles.length;
+    tiles.forEach(t => {
+        const x = t.x;
+        const y = t.y;
+        const z = t.z;
+        const mvt_extent = 1024;
+        const subpixelBufferSize = 0;
+        const query =
+            `select st_asmvt(geom, 'lid') FROM
+        (
+            SELECT
+                ST_AsMVTGeom(
+                    ST_SetSRID(ST_MakePoint(avg(ST_X(the_geom_webmercator)), avg(ST_Y(the_geom_webmercator))),3857),
+                    CDB_XYZ_Extent(${x},${y},${z}), ${mvt_extent}, ${subpixelBufferSize}, false
+                ),
+                SUM(amount) AS amount
+            FROM tx_0125_copy_copy AS cdbq
+            WHERE the_geom_webmercator && CDB_XYZ_Extent(${x},${y},${z})
+            GROUP BY ST_SnapToGrid(the_geom_webmercator, CDB_XYZ_Resolution(${z})*3.)
+            ORDER BY amount DESC
+        )AS geom
+    `;
+        var oReq = new XMLHttpRequest();
+        oReq.open("GET", "https://dmanzanares-core.carto.com/api/v2/sql?q=" + encodeURIComponent(query) + "", true);
+        oReq.onload = function (oEvent) {
+            const json = JSON.parse(oReq.response);
+            if (json.rows[0].st_asmvt.data.length == 0) {
+                needToComplete--;
+                if (completedTiles.length == needToComplete) {
+                    oldtiles.forEach(t => renderer.removeDataframe(t));
+                    completedTiles.forEach(f => renderer.addDataframe(f).setStyle(style));
+                    oldtiles = completedTiles;
+                    styleWidth();
+                    styleColor();
+                }
+                return;
+            }
+            var tile = new VectorTile(new Protobuf(new Uint8Array(json.rows[0].st_asmvt.data)));
+            const mvtLayer = tile.layers[Object.keys(tile.layers)[0]];
+            var fieldMap = {
+                category: 0,
+                amount: 1
+            };
+            var properties = [[new Float32Array(mvtLayer.length)], [new Float32Array(mvtLayer.length)]];
+            var points = new Float32Array(mvtLayer.length * 2);
+            const r = Math.random();
+            for (var i = 0; i < mvtLayer.length; i++) {
+                const f = mvtLayer.feature(i);
+                const geom = f.loadGeometry();
+                points[2 * i + 0] = 2 * (geom[0][0].x) / mvt_extent - 1.;
+                points[2 * i + 1] = 2 * (1. - (geom[0][0].y) / mvt_extent) - 1.;
+                properties[0][i] = Number(Math.random());
+                //properties[1][i] = Number(Math.random());
+                properties[1][i] = Number(f.properties.amount);
+                //console.log(f);
+                //break;
+            }
+            console.log(`dataframe feature count: ${mvtLayer.length} ${x},${y},${z}`);
+            var dataframe = {
+                center: { x: ((x + 0.5) / Math.pow(2, z)) * 2. - 1, y: (1. - (y + 0.5) / Math.pow(2, z)) * 2. - 1. },
+                scale: 1 / Math.pow(2, z),
+                geom: points,
+                properties: {},
+            };
+            Object.keys(fieldMap).map((name, pid) => {
+                dataframe.properties[name] = properties[pid];
+            });
+            dataframe.schema = new R.Schema(Object.keys(dataframe.properties), Object.keys(dataframe.properties).map(() => 'float'));
+            console.log(Object.keys(dataframe.properties), Object.keys(dataframe.properties).map(() => 'float'), dataframe.schema);
+            completedTiles.push(dataframe);
+            if (completedTiles.length == needToComplete) {
+                oldtiles.forEach(t => renderer.removeDataframe(t));
+                completedTiles.forEach(f => renderer.addDataframe(f).setStyle(style));
+                oldtiles = completedTiles;
+                styleWidth();
+                styleColor();
+            }
+        };
+        oReq.send(null);
+    });
+}
+
+const DEG2RAD = Math.PI / 180;
+const EARTH_RADIUS = 6378137;
+const WM_R = EARTH_RADIUS * Math.PI; // Webmercator *radius*: half length Earth's circumference
+const WM_2R = WM_R * 2; // Webmercator coordinate range (Earth's circumference)
+const TILE_SIZE = 256;
+// Webmercator projection
+function Wmxy(latLng) {
+    let lat = latLng.lat * DEG2RAD;
+    let lng = latLng.lng * DEG2RAD;
+    let x = lng * EARTH_RADIUS;
+    let y = Math.log(Math.tan(lat / 2 + Math.PI / 4)) * EARTH_RADIUS;
+    return { x: x, y: y };
+}
+
+var mapboxgl = window.mapboxgl;
+mapboxgl.accessToken = 'pk.eyJ1IjoiZG1hbnphbmFyZXMiLCJhIjoiY2o5cHRhOGg5NWdzbTJxcXltb2g2dmE5NyJ9.RVto4DnlLzQc26j9H0g9_A';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/dark-v9', // stylesheet location
+    center: [2.17, 41.38], // starting position [lng, lat]
+    zoom: 14, // starting zoom,
+});
+map.repaint = false;
+function getZoom() {
+    var b = map.getBounds();
+    var c = map.getCenter();
+    var nw = b.getNorthWest();
+    var sw = b.getSouthWest();
+    var z = (Wmxy(nw).y - Wmxy(sw).y) / WM_2R;
+    renderer.setCenter(c.lng / 180., Wmxy(c).y / WM_R);
+    return z;
+}
+
+map.on('load', _ => {
+    var cont = map.getCanvasContainer();
+    var canvas = document.createElement('canvas')
+    canvas.id = 'good';
+    cont.appendChild(canvas)
+    canvas.style.width = map.getCanvas().style.width;
+    canvas.style.height = map.getCanvas().style.height;
+
+    function move() {
+        var b = map.getBounds();
+        var nw = b.getNorthWest();
+        var c = map.getCenter();
+
+        renderer.setCenter(c.lng / 180., Wmxy(c).y / WM_R);
+        renderer.setZoom(getZoom());
+
+        c = renderer.getCenter();
+        var z = renderer.getZoom();
+    }
+    function moveEnd() {
+        move();
+        getData(canvas.clientWidth / canvas.clientHeight);
     };
-
-    var gl = canvas.getContext('webgl');
-    if (!gl) {
-      return support;
-    } else {
-      support.webgl1 = true;
+    function resize() {
+        canvas.style.width = map.getCanvas().style.width;
+        canvas.style.height = map.getCanvas().style.height;
+        move();
     }
 
-    var OES_texture_float = gl.getExtension("OES_texture_float");
-    support.OES_texture_float = OES_texture_float ? true : false;
+    renderer = new R.Renderer(canvas);
+    schema = new R.Schema(['category', 'amount'], ['float', 'float']);
+    style = new R.Style.Style(renderer, schema);
+    $('#widthStyleEntry').on('input', styleWidth);
+    $('#colorStyleEntry').on('input', styleColor);
+    resize();
+    moveEnd();
 
-
-    var EXT_blend_minmax = gl.getExtension('EXT_blend_minmax');
-    support.EXT_blend_minmax = EXT_blend_minmax ? true : false;
-
-    support.MAX_RENDERBUFFER_SIZE = gl.getParameter(gl.MAX_RENDERBUFFER_SIZE);
-
-    var aux1x1FB = gl.createFramebuffer();
-    var aux1x1TEX = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, aux1x1TEX);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,
-      1, 1, 0, gl.RGBA, gl.FLOAT,
-      null);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, aux1x1FB);
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, aux1x1TEX, 0);
-    //Check FB completeness
-    support.renderToFloat = getFBstatus(gl);
-    return support;
-  }
-
-  function trackWebGLSupport(){
-    if (Math.random() < 10.05) {
-      var testCanvas = document.createElement("canvas");
-      testCanvas.style.position = 'absolute';
-      testCanvas.style.pointerEvents = 'none';
-      document.body.appendChild(testCanvas);
-      try {
-        var support = getWebGLsupport(testCanvas);
-        var str = JSON.stringify(support);
-        console.log(str);
-        //alert(str)
-        //trackjs.log(str);
-      } catch (error) {
-        console.log(error);
-        //trackjs.error(error);
-      }
-    }
-  }
-  trackWebGLSupport();
-  console.timeEnd('test');
-
+    map.on('resize', resize);
+    map.on('movestart', move);
+    map.on('move', move);
+    map.on('moveend', moveEnd);
+});
