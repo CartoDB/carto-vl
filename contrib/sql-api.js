a9e7883fb280559bfa923aa4bf2bb57ce0beff1d
David M <dmanzanares@cartodb.com>
2017-12-04 14:45:47 +0100

    SQL-API getSchema() PoC

--- contrib/sql-api.js
+++ contrib/sql-api.js
@@ -8,10 +8,11 @@ export { getData, schema, init };
 
 var oldtiles = [];
 var ajax;
-const names =['Moda y calzado',
-'Bares y restaurantes', 'Salud', 'Alimentación'];
+const names = ['Moda y calzado',
+    'Bares y restaurantes', 'Salud', 'Alimentación'];
 var schema = new R.schema.Schema(['category', 'amount'], [new R.schema.Category(names
     , [33263, 24633, 17833, 16907], [0, 1, 2, 3]), new R.schema.Float(2, 100 * 1000)]);
+getSchema();
 var style;
 
 function init(fixedStyle) {
@@ -28,6 +29,73 @@ function getCatID(catStr) {
     }
     catMap[catStr] = Object.keys(catMap).length + 1;
     return catMap[catStr];
+}
+
+async function getColumnTypes() {
+    const columnListQuery = `select * from tx_0125_copy_copy limit 0;`;
+    const response = await fetch("https://dmanzanares-core.carto.com/api/v2/sql?q=" + encodeURIComponent(columnListQuery));
+    const json = await response.json();
+    return json.fields;
+}
+
+async function getNumericTypes(names) {
+    const aggFns = ['min', 'max', 'sum', 'avg'];
+    const numericsSelect = names.map(name =>
+        aggFns.map(fn => `${fn}(${name}) AS ${name}_${fn}`)
+    ).concat(['COUNT(*)']).join();
+    const numericsQuery = `SELECT ${numericsSelect} FROM tx_0125_copy_copy;`
+    const response = await fetch("https://dmanzanares-core.carto.com/api/v2/sql?q=" + encodeURIComponent(numericsQuery));
+    const json = await response.json();
+    console.log(numericsQuery, json);
+    // TODO avg, sum, count
+    return names.map(name =>
+        new R.schema.Float(json.rows[0][`${name}_min`], json.rows[0][`${name}_max`])
+    );
+}
+
+async function getCategoryTypes(names) {
+    const aggFns = ['min', 'max', 'sum', 'avg'];
+    const numericsSelect = names.map(name =>
+        aggFns.map(fn => `${fn}(${name}) AS ${name}_${fn}`)
+    ).concat(['COUNT(*)']).join();
+    const numericsQuery = `SELECT ${numericsSelect} FROM tx_0125_copy_copy;`
+    const response = await fetch("https://dmanzanares-core.carto.com/api/v2/sql?q=" + encodeURIComponent(numericsQuery));
+    const json = await response.json();
+    console.log(numericsQuery, json);
+
+    return names.map(name => {
+        console.log("ASD", json.rows[0][`${name}_min`])
+        return new R.schema.Float(json.rows[0][`${name}_min`], json.rows[0][`${name}_max`])
+    }
+    );
+}
+
+
+async function getSchema() {
+    //Get column names and types with a limit 0
+    //Get min,max,sum and count of numerics
+    //for each category type
+    //Get category names and counts by grouping by
+    //Assign ids
+
+    const fields = await getColumnTypes();
+    let numerics = [];
+    let categories = [];
+    Object.keys(fields).map(name => {
+        const type = fields[name].type;
+        if (type == 'number') {
+            numerics.push(name);
+        } else if (type == 'string') {
+            categories.push(name);
+        }
+    })
+
+    const numericsTypes = await getNumericTypes(numerics);
+    const categoriesTypes = [];//await getCategoryTypes(categories);
+
+    const schema = new R.schema.Schema(numerics.concat([]), numericsTypes.concat(categoriesTypes));
+    console.log(schema, numericsTypes);
+    return schema;
 }
 
 function getData(renderer) {
