be4dbd6917435f93c6d1312c8e44bbd651c95442
David Manzanares <dmanzanares@cartodb.com>
2017-12-11 17:12:04 +0100

    Demo improved

--- example/mapbox.js
+++ example/mapbox.js
@@ -11,6 +11,89 @@ var map = new mapboxgl.Map({
 map.repaint = false;
 var mgl = new MGL.MGLIntegrator(map);
 
+$('#barcelona').click(() => {
+    $('.step').css('display', 'inline');
+    $('#styleEntry').removeClass('twelve columns').addClass('eight columns');
+    document.getElementById("styleEntry").value = 'width:    40*(($amount/max($amount))^0.5) * (zoom()/10000 + 0.01)\ncolor:    ramp($category, Prism)';
+    mgl.provider.setQueries(...mgl.barcelona());
+    mgl.provider.getSchema().then(schema => {
+        mgl.schema = schema;
+        mgl.updateStyle();
+    });
+});
+
+$('#wwi').click(() => {
+    $('.step').css('display', 'none');
+    $('#styleEntry').removeClass('eight columns').addClass('twelve columns');
+    document.getElementById("styleEntry").value = 'width:    blend(1,2,near($day, (25*now()) %1000, 0, 10), cubic) *zoom()\ncolor:    setopacity(ramp($temp, tealrose, 0, 30), blend(0.005,1,near($day, (25*now()) %1000, 0, 10), cubic))';
+    mgl.provider.setQueries(...mgl.ships_WWI());
+    mgl.provider.getSchema().then(schema => {
+        mgl.schema = schema;
+        mgl.updateStyle();
+    });
+});
+
+$('.step').css('display', 'none');
+$('#styleEntry').removeClass('eight columns').addClass('twelve columns');
+
+
+const styles = [
+    `width: 3
+color: rgba(0.8,0,0,1)`,
+
+    `width: 3
+color: rgba(0.8,0,0,0.2)`,
+
+    `width: 3
+color: hsv(0, 0, 1)`,
+
+    `width: 3
+color: hsv(0, 0.7, 1.)`,
+
+    `width: 3
+color: hsv(0.2, 0.7, 1.)`,
+
+    `width: 3
+color: hsv(0.7, 0.7, 1.)`,
+
+    `width: 3
+color: hsv($category/10, 0.7, 1.)`,
+
+    `width: 3
+color: ramp($category, Prism)`,
+
+    `width: 3
+color: ramp(top($category, 4), Prism)`,
+
+    `width: 3
+color: setOpacity( ramp($category, Prism), $amount/max($amount))`,
+
+    `width: 3
+color: ramp($category, Prism)`,
+
+    `width: sqrt($amount/50000)*20
+color: ramp($category, Prism)`,
+
+    `width: sqrt($amount/50000)*20*(zoom()/4000+0.01)*1.5
+color: ramp($category, Prism)`,
+];
+const texts = [];
+
+let index = -1;
+
+$('#prev').click(() => {
+    if (index > 0) {
+        index--;
+        mgl.setStyle(styles[index]);
+    }
+});
+$('#next').click(() => {
+    if (index < styles.length - 1) {
+        index++;
+        mgl.setStyle(styles[index]);
+    }
+});
+
 /*
 var map2 = new mapboxgl.Map({
     container: 'map2', // container id
