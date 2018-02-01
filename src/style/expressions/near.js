f929c3db50076e2f302a58686985e0cb977c883c
IagoLast <iago.lastra@gmail.com>
2018-02-01 09:16:41 +0100

    Split unary, binary and interpolations

--- src/style/expressions/near.js
+++ src/style/expressions/near.js
@@ -1,22 +1,16 @@
 import Expression from './expression';
 import { implicitCast } from './utils';
 
-<<<<<<< HEAD
 //
-=======
->>>>>>> 97d9d4de6f598fb1f2f1b8125e0fcebc4c5c6653
 export default class Near extends Expression {
     /**
      * @api
      * @description Near returns zero for inputs that are far away from center.
      * This can be useful for filtering out features by setting their size to zero.
-<<<<<<< HEAD
      * 
      *       _____
      * _____/     \_____
      * 
-=======
->>>>>>> 97d9d4de6f598fb1f2f1b8125e0fcebc4c5c6653
      * @param {*} input
      * @param {*} center
      * @param {*} threshold size of the allowed distance between input and center that is filtered in (returning one)
--- src/style/functions.js
+++ src/style/functions.js
@@ -1,10 +1,8 @@
-import { implicitCast, genBinaryOp, genUnaryOp } from './expressions/utils';
 import { palettes } from './expressions/palettes';
 import Animate from './expressions/animate';
 import Blend from './expressions/blend';
 import Buckets from './expressions/buckets';
 import CIELab from './expressions/CIELab';
-import Expression from './expressions/expression';
 import Float from './expressions/float';
 import HSV from './expressions/hsv';
 import Near from './expressions/near';
@@ -18,148 +16,82 @@ import XYZ from './expressions/xyz';
 import Zoom from './expressions/zoom';
 
 // Unary ops
-import { Log } from './unary';
-import { Sqrt } from './unary';
-import { Sin } from './unary';
-import { Cos } from './unary';
-import { Tan } from './unary';
-import { Sign } from './unary';
-import { Abs } from './unary';
-
-const genAggregationOp = (aggName) => class AggregationOperation extends Expression {
-    constructor(property) {
-        super({ property: property });
-    }
-    get name() {
-        return this.property.name;
-    }
-    get numCategories() {
-        return this.property.numCategories;
-    }
-    //Override super methods, we don't want to let the property use the raw column, we must use the agg suffixed one
-    _compile(metadata) {
-        super._compile(metadata);
-        this.type = this.property.type;
-    }
-    _applyToShaderSource(uniformIDMaker, propertyTIDMaker) {
-        return {
-            preface: '',
-            inline: `p${propertyTIDMaker(`_cdb_agg_${aggName}_${this.property.name}`)}`
-        };
-    }
-    _postShaderCompile() { }
-    _getMinimumNeededSchema() {
-        return {
-            columns: [
-                `_cdb_agg_${aggName}_${this.property.name}`
-            ]
-        };
-    }
-};
-
-// Aggregation ops
-const Max = genAggregationOp('max');
-const Min = genAggregationOp('min');
-const Avg = genAggregationOp('avg');
-const Sum = genAggregationOp('sum');
-const Mode = genAggregationOp('mode');
+import { Log } from './expressions/unary';
+import { Sqrt } from './expressions/unary';
+import { Sin } from './expressions/unary';
+import { Cos } from './expressions/unary';
+import { Tan } from './expressions/unary';
+import { Sign } from './expressions/unary';
+import { Abs } from './expressions/unary';
 
 // Binary ops
-class FloatMul extends genBinaryOp((x, y) => x * y, (x, y) => `(${x} * ${y})`) { }
-const FloatDiv = genBinaryOp((x, y) => x / y, (x, y) => `(${x} / ${y})`);
-const FloatAdd = genBinaryOp((x, y) => x + y, (x, y) => `(${x} + ${y})`);
-const FloatSub = genBinaryOp((x, y) => x - y, (x, y) => `(${x} - ${y})`);
-const FloatMod = genBinaryOp((x, y) => x % y, (x, y) => `mod(${x}, ${y})`);
-const FloatPow = genBinaryOp((x, y) => Math.pow(x, y), (x, y) => `pow(${x}, ${y})`);
+import { FloatMul } from './expressions/binary';
+import { FloatDiv } from './expressions/binary';
+import { FloatAdd } from './expressions/binary';
+import { FloatSub } from './expressions/binary';
+import { FloatMod } from './expressions/binary';
+import { FloatPow } from './expressions/binary';
+import { GreaterThan } from './expressions/binary';
+import { GreaterThanOrEqualTo } from './expressions/binary';
+import { LessThan } from './expressions/binary';
+import { LessThanOrEqualTo } from './expressions/binary';
+import { Equals } from './expressions/binary';
+import { NotEquals } from './expressions/binary';
 
-const GreaterThan = genBinaryOp((x, y) => x > y ? 1 : 0, (x, y) => `(${x}>${y}? 1.:0.)`);
-const GreaterThanOrEqualTo = genBinaryOp((x, y) => x >= y ? 1 : 0, (x, y) => `(${x}>=${y}? 1.:0.)`);
-
-const LessThan = genBinaryOp((x, y) => x < y ? 1 : 0, (x, y) => `(${x}<${y}? 1.:0.)`);
-const LessThanOrEqualTo = genBinaryOp((x, y) => x <= y ? 1 : 0, (x, y) => `(${x}<=${y}? 1.:0.)`);
-
-const Equals = genBinaryOp((x, y) => x == y ? 1 : 0, (x, y) => `(${x}==${y}? 1.:0.)`);
-const NotEquals = genBinaryOp((x, y) => x != y ? 1 : 0, (x, y) => `(${x}!=${y}? 1.:0.)`);
-
-
+// Aggregation ops
+import { Max } from './expressions/aggregation';
+import { Min } from './expressions/aggregation';
+import { Avg } from './expressions/aggregation';
+import { Sum } from './expressions/aggregation';
+import { Mode } from './expressions/aggregation';
 
 // Interpolators
-const genInterpolator = (inlineMaker, preface) => class Interpolator extends Expression {
-    constructor(m) {
-        m = implicitCast(m);
-        super({ m: m });
-        this.isInterpolator = true; //TODO remove this hack
-    }
-    _compile(meta) {
-        super._compile(meta);
-        if (this.m.type != 'float') {
-            throw new Error(`Blending cannot be performed by '${this.m.type}'`);
-        }
-        this.type = 'float';
-        this._setGenericGLSL(inline => inlineMaker(inline.m), preface);
-    }
-};
-class Linear extends genInterpolator(inner => inner) { }
-class Cubic extends genInterpolator(inner => `cubicEaseInOut(${inner})`,
-    `
-    #ifndef CUBIC
-    #define CUBIC
-    float cubicEaseInOut(float p){
-        if (p < 0.5) {
-            return 4. * p * p * p;
-        }else {
-            float f = ((2. * p) - 2.);
-            return 0.5 * f * f * f + 1.;
-        }
-    }
-    #endif
-`) { }
+import { Linear } from './expressions/interpolators';
+import { Cubic } from './expressions/interpolators';
 
 
 
-const floatMul = (...args) => new FloatMul(...args);
-const floatDiv = (...args) => new FloatDiv(...args);
-const floatAdd = (...args) => new FloatAdd(...args);
-const floatSub = (...args) => new FloatSub(...args);
-const floatPow = (...args) => new FloatPow(...args);
-const floatMod = (...args) => new FloatMod(...args);
-const log = (...args) => new Log(...args);
-const sqrt = (...args) => new Sqrt(...args);
-const sin = (...args) => new Sin(...args);
-const cos = (...args) => new Cos(...args);
-const tan = (...args) => new Tan(...args);
-const sign = (...args) => new Sign(...args);
-const near = (...args) => new Near(...args);
-const blend = (...args) => new Blend(...args);
-const rgba = (...args) => new RGBA(...args);
-const property = (...args) => new Property(...args);
-const animate = (...args) => new Animate(...args);
-const hsv = (...args) => new HSV(...args);
-const setOpacity = (...args) => new Opacity(...args);
-const opacity = (...args) => new Opacity(...args);
-const ramp = (...args) => new Ramp(...args);
-const float = (...args) => new Float(...args);
-const max = (...args) => new Max(...args);
-const min = (...args) => new Min(...args);
-const sum = (...args) => new Sum(...args);
-const avg = (...args) => new Avg(...args);
-const mode = (...args) => new Mode(...args);
-const top = (...args) => new Top(...args);
-const linear = (...args) => new Linear(...args);
-const cubic = (...args) => new Cubic(...args);
-const now = (...args) => new Now(...args);
-const zoom = (...args) => new Zoom(...args);
-const cielab = (...args) => new CIELab(...args);
-const xyz = (...args) => new XYZ(...args);
-const abs = (...args) => new Abs(...args);
-const greaterThan = (...args) => new GreaterThan(...args);
-const greaterThanOrEqualTo = (...args) => new GreaterThanOrEqualTo(...args);
-const lessThan = (...args) => new LessThan(...args);
-const lessThanOrEqualTo = (...args) => new LessThanOrEqualTo(...args);
-const equals = (...args) => new Equals(...args);
-const notEquals = (...args) => new NotEquals(...args);
-const buckets = (...args) => new Buckets(...args);
-
-export {
-    palettes, property, blend, now, near, rgba, float, ramp, floatMul, floatDiv, floatAdd, floatSub, floatPow, log, sqrt, sin, cos, tan, sign, setOpacity, opacity, hsv, animate, max, min, top, linear, cubic, zoom, floatMod, cielab, xyz, abs, greaterThan, greaterThanOrEqualTo, lessThan, lessThanOrEqualTo, equals, notEquals, buckets, avg, sum, mode
-};
+// Expose clases as constructor functions
+export const floatMul = (...args) => new FloatMul(...args);
+export const floatDiv = (...args) => new FloatDiv(...args);
+export const floatAdd = (...args) => new FloatAdd(...args);
+export const floatSub = (...args) => new FloatSub(...args);
+export const floatPow = (...args) => new FloatPow(...args);
+export const floatMod = (...args) => new FloatMod(...args);
+export const log = (...args) => new Log(...args);
+export const sqrt = (...args) => new Sqrt(...args);
+export const sin = (...args) => new Sin(...args);
+export const cos = (...args) => new Cos(...args);
+export const tan = (...args) => new Tan(...args);
+export const sign = (...args) => new Sign(...args);
+export const near = (...args) => new Near(...args);
+export const blend = (...args) => new Blend(...args);
+export const rgba = (...args) => new RGBA(...args);
+export const property = (...args) => new Property(...args);
+export const animate = (...args) => new Animate(...args);
+export const hsv = (...args) => new HSV(...args);
+export const setOpacity = (...args) => new Opacity(...args);
+export const opacity = (...args) => new Opacity(...args);
+export const ramp = (...args) => new Ramp(...args);
+export const float = (...args) => new Float(...args);
+export const max = (...args) => new Max(...args);
+export const min = (...args) => new Min(...args);
+export const sum = (...args) => new Sum(...args);
+export const avg = (...args) => new Avg(...args);
+export const mode = (...args) => new Mode(...args);
+export const top = (...args) => new Top(...args);
+export const linear = (...args) => new Linear(...args);
+export const cubic = (...args) => new Cubic(...args);
+export const now = (...args) => new Now(...args);
+export const zoom = (...args) => new Zoom(...args);
+export const cielab = (...args) => new CIELab(...args);
+export const xyz = (...args) => new XYZ(...args);
+export const abs = (...args) => new Abs(...args);
+export const greaterThan = (...args) => new GreaterThan(...args);
+export const greaterThanOrEqualTo = (...args) => new GreaterThanOrEqualTo(...args);
+export const lessThan = (...args) => new LessThan(...args);
+export const lessThanOrEqualTo = (...args) => new LessThanOrEqualTo(...args);
+export const equals = (...args) => new Equals(...args);
+export const notEquals = (...args) => new NotEquals(...args);
+export const buckets = (...args) => new Buckets(...args);
+export { palettes };
