9fba3f3433346ef69f7f06edc6b532390b4ce500
IagoLast <iago.lastra@gmail.com>
2018-02-05 17:34:26 +0100

    Split requestDataframe function

--- contrib/windshaft-sql.js
+++ contrib/windshaft-sql.js
@@ -221,89 +221,15 @@ export default class WindshaftSQL extends Provider {
                         catFieldsReal.map((name, i) => fieldMap[name] = i);
                         numFieldsReal.map((name, i) => fieldMap[name] = i + catFields.length);
 
-                        var properties = [new Float32Array(mvtLayer.length + 1024), new Float32Array(mvtLayer.length + 1024), new Float32Array(mvtLayer.length + 1024), new Float32Array(mvtLayer.length + 1024)];
-                        if (this.geomType == 'point') {
-                            var points = new Float32Array(mvtLayer.length * 2);
-                        }
-                        let featureGeometries = [];
-                        for (var i = 0; i < mvtLayer.length; i++) {
-                            const f = mvtLayer.feature(i);
-                            const geom = f.loadGeometry();
-                            let geometry = [];
-                            if (this.geomType == 'point') {
-                                points[2 * i + 0] = 2 * (geom[0][0].x) / mvt_extent - 1.;
-                                points[2 * i + 1] = 2 * (1. - (geom[0][0].y) / mvt_extent) - 1.;
-                            } else if (this.geomType == 'polygon') {
-                                let polygon = null;
-                                /*
-                                    All this clockwise non-sense is needed because the MVT decoder dont decode the MVT fully.
-                                    It doesn't distinguish between internal polygon rings (which defines holes) or external ones, which defines more polygons (mulipolygons)
-                                    See:
-                                        https://github.com/mapbox/vector-tile-spec/tree/master/2.1
-                                        https://en.wikipedia.org/wiki/Shoelace_formula
-                                */
-                                for (let j = 0; j < geom.length; j++) {
-                                    //if exterior
-                                    //   push current polygon & set new empty
-                                    //else=> add index to holes
-                                    if (isClockWise(geom[j])) {
-                                        if (polygon) {
-                                            geometry.push(polygon);
-                                        }
-                                        polygon = {
-                                            flat: [],
-                                            holes: []
-                                        };
-                                    } else {
-                                        if (j == 0) {
-                                            throw new Error('Invalid MVT tile: first polygon ring MUST be external');
-                                        }
-                                        polygon.holes.push(polygon.flat.length / 2);
-                                    }
-                                    for (let k = 0; k < geom[j].length; k++) {
-                                        polygon.flat.push(2 * geom[j][k].x / mvt_extent - 1.);
-                                        polygon.flat.push(2 * (1. - geom[j][k].y / mvt_extent) - 1.);
-                                    }
-                                }
-                                //if current polygon is not empty=> push it
-                                if (polygon && polygon.flat.length > 0) {
-                                    geometry.push(polygon);
-                                }
-                                featureGeometries.push(geometry);
-                            } else if (this.geomType == 'line') {
-                                geom.map(l => {
-                                    let line = [];
-                                    l.map(point => {
-                                        line.push(2 * point.x / mvt_extent - 1, 2 * (1 - point.y / mvt_extent) - 1);
-                                    });
-                                    geometry.push(line);
-                                });
-                                featureGeometries.push(geometry);
-                            } else {
-                                throw new Error(`Unimplemented geometry type: '${this.geomType}'`);
-                            }
-
-                            catFields.map((name, index) => {
-                                properties[index][i] = this.getCatID(name, f.properties[name], metadata, catFieldsReal[index]);
-                            });
-                            numFields.map((name, index) => {
-                                properties[index + catFields.length][i] = Number(f.properties[name]);
-                            });
-                        }
+                        const { points, featureGeometries, properties } = this._decodeMVTLayer(mvtLayer, metadata, mvt_extent, catFields, catFieldsReal, numFields);
 
                         var rs = rsys.getRsysFromTile(x, y, z);
                         let dataframeProperties = {};
                         Object.keys(fieldMap).map((name, pid) => {
                             dataframeProperties[name] = properties[pid];
                         });
-                        var dataframe = new R.Dataframe(
-                            rs.center,
-                            rs.scale,
-                            this.geomType == 'point' ? points : featureGeometries,
-                            dataframeProperties,
-                        );
-                        dataframe.type = this.geomType;
-                        dataframe.size = mvtLayer.length;
+                        let dataFrameGeometry = this.geomType == 'point' ? points : featureGeometries;
+                        const dataframe = this._generateDataFrame(rs, dataFrameGeometry, dataframeProperties, mvtLayer.length, this.geomType);
                         this.renderer.addDataframe(dataframe);
                         callback(dataframe);
                     });
@@ -311,6 +237,93 @@ export default class WindshaftSQL extends Provider {
                 oReq.send(null);
             });
         });
+    }
+
+    _decodeMVTLayer(mvtLayer, metadata, mvt_extent, catFields, catFieldsReal, numFields) {
+        var properties = [new Float32Array(mvtLayer.length + 1024), new Float32Array(mvtLayer.length + 1024), new Float32Array(mvtLayer.length + 1024), new Float32Array(mvtLayer.length + 1024)];
+        if (this.geomType == 'point') {
+            var points = new Float32Array(mvtLayer.length * 2);
+        }
+        let featureGeometries = [];
+        for (var i = 0; i < mvtLayer.length; i++) {
+            const f = mvtLayer.feature(i);
+            const geom = f.loadGeometry();
+            let geometry = [];
+            if (this.geomType == 'point') {
+                points[2 * i + 0] = 2 * (geom[0][0].x) / mvt_extent - 1.;
+                points[2 * i + 1] = 2 * (1. - (geom[0][0].y) / mvt_extent) - 1.;
+            } else if (this.geomType == 'polygon') {
+                let polygon = null;
+                /*
+                    All this clockwise non-sense is needed because the MVT decoder dont decode the MVT fully.
+                    It doesn't distinguish between internal polygon rings (which defines holes) or external ones, which defines more polygons (mulipolygons)
+                    See:
+                        https://github.com/mapbox/vector-tile-spec/tree/master/2.1
+                        https://en.wikipedia.org/wiki/Shoelace_formula
+                */
+                for (let j = 0; j < geom.length; j++) {
+                    //if exterior
+                    //   push current polygon & set new empty
+                    //else=> add index to holes
+                    if (isClockWise(geom[j])) {
+                        if (polygon) {
+                            geometry.push(polygon);
+                        }
+                        polygon = {
+                            flat: [],
+                            holes: []
+                        };
+                    } else {
+                        if (j == 0) {
+                            throw new Error('Invalid MVT tile: first polygon ring MUST be external');
+                        }
+                        polygon.holes.push(polygon.flat.length / 2);
+                    }
+                    for (let k = 0; k < geom[j].length; k++) {
+                        polygon.flat.push(2 * geom[j][k].x / mvt_extent - 1.);
+                        polygon.flat.push(2 * (1. - geom[j][k].y / mvt_extent) - 1.);
+                    }
+                }
+                //if current polygon is not empty=> push it
+                if (polygon && polygon.flat.length > 0) {
+                    geometry.push(polygon);
+                }
+                featureGeometries.push(geometry);
+            } else if (this.geomType == 'line') {
+                geom.map(l => {
+                    let line = [];
+                    l.map(point => {
+                        line.push(2 * point.x / mvt_extent - 1, 2 * (1 - point.y / mvt_extent) - 1);
+                    });
+                    geometry.push(line);
+                });
+                featureGeometries.push(geometry);
+            } else {
+                throw new Error(`Unimplemented geometry type: '${this.geomType}'`);
+            }
+
+            catFields.map((name, index) => {
+                properties[index][i] = this.getCatID(name, f.properties[name], metadata, catFieldsReal[index]);
+            });
+            numFields.map((name, index) => {
+                properties[index + catFields.length][i] = Number(f.properties[name]);
+            });
+        }
+
+        return { properties, points, featureGeometries };
+    }
+
+    _generateDataFrame(rs, geometry, properties, size, type) {
+        const dataframe = new R.Dataframe(
+            rs.center,
+            rs.scale,
+            geometry,
+            properties,
+        );
+        dataframe.type = type;
+        dataframe.size = size;
+
+        return dataframe;
     }
     getData() {
         if (!this.dataset) {
