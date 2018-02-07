e9b61d45c255100133a038c934169a52ad37b357
David M <dmanzanares@cartodb.com>
2018-02-07 18:30:23 +0100

    Fix webpack linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
+/*eslint-env node*/
 const path = require('path');
 const webpack = require('webpack');
 const VERSION = require('./package.json').version;
 const banner = `CARTOGL-${VERSION}`;
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'cartogl.js',
-    library: 'carto',
-    libraryTarget: 'umd'
-  },
-  devtool: 'sourcemap',
-  plugins: [
-    new webpack.BannerPlugin(banner)
-  ]
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'cartogl.js',
+        library: 'carto',
+        libraryTarget: 'umd'
+    },
+    devtool: 'sourcemap',
+    plugins: [
+        new webpack.BannerPlugin(banner)
+    ]
 };
