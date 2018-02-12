4938c38a48d496d5de9565a39ad1c3c37cbc9e1a
IagoLast <iago.lastra@gmail.com>
2018-02-12 18:13:05 +0100

    Rename test folder and add chai-as-promised

--- test/acceptance/index.test.js
+++ test/acceptance/index.test.js
@@ -1,12 +1,15 @@
 const serve = require('serve');
 const exquisite = require('exquisite-sst');
 const path = require('path');
-// const expect = require('chai').expect;
-// const fs = require('fs');
-const DELAY = 5000;
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
 
-describe('Basic tests', () => {
-    let server;
+const expect = chai.expect;
+const DELAY = 3000;
+var server;
+
+
+describe('Screenshot tests:', () => {
     before(done => {
         server = serve(path.join(__dirname, '../../'), { port: 5555, });
         done();
@@ -15,9 +18,11 @@ describe('Basic tests', () => {
         const input = path.resolve(__dirname, 'reference/single-layer.png');
         const output = path.resolve(__dirname, 'reference/single-layer_out.png');
         const URL = 'http://localhost:5000/example/basic/single-layer.html';
-        return exquisite.test({ input, output, url: URL, delay: DELAY });
+        return expect(exquisite.test({ input, output, url: URL, delay: DELAY })).to.eventually.be.true;
     });
+
     after(() => {
         server.stop();
     });
 });
+
