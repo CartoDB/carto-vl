c09224830aac41220fea277fc98f189e14a9ebe7
IagoLast <iago.lastra@gmail.com>
2018-02-13 10:43:36 +0100

    Automate test to run all examples

---
+++ test/acceptance/get-references.js
@@ -0,0 +1,26 @@
+
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const serve = require('serve');
+
+const server = serve(path.join(__dirname, '../../'), { port: 5556, });
+const FOLDERS = ['basic', 'styling'];
+
+FOLDERS.forEach(getReferences);
+
+server.stop();
+
+
+/**
+ * Uses exquisite-sst from the command line to take screenshots of all html files on a folder
+ * @param {*} folder 
+ */
+function getReferences(folder) {
+    const files = fs.readdirSync(path.join(__dirname, `../../example/${folder}`));
+    files.forEach(file => {
+        if (file.endsWith('.html')) {
+            execSync(`$(npm bin)/exquisite-sst --reference --url http://localhost:5000/example/${folder}/${file} --output ./test/acceptance/reference/${folder}/${file.replace('.html', '.png')} --delay 8000`);
+        }
+    });
+}
--- test/acceptance/index.test.js
+++ test/acceptance/index.test.js
@@ -1,4 +1,5 @@
 const serve = require('serve');
+const fs = require('fs');
 const exquisite = require('exquisite-sst');
 const path = require('path');
 const chai = require('chai');
@@ -8,21 +9,48 @@ const expect = chai.expect;
 const DELAY = 3000;
 var server;
 
+const references = _flatten(_getReferences('reference'));
+
 
 describe('Screenshot tests:', () => {
     before(done => {
         server = serve(path.join(__dirname, '../../'), { port: 5555, });
         done();
     });
-    it('single-layer', () => {
-        const input = path.resolve(__dirname, 'reference/single-layer.png');
-        const output = path.resolve(__dirname, 'reference/single-layer_out.png');
-        const URL = 'http://localhost:5000/example/basic/single-layer.html';
-        return expect(exquisite.test({ input, output, url: URL, delay: DELAY })).to.eventually.be.true;
-    });
-
+    references.forEach(test);
     after(() => {
         server.stop();
     });
 });
 
+// Wrapper to perform a single test.
+function test({ folder, file }) {
+    file = file.replace('.png', '');
+    it(file, () => {
+        const input = path.resolve(__dirname, `reference/${folder}/${file}.png`);
+        const output = path.resolve(__dirname, `reference/${folder}/${file}_out.png`);
+        const URL = `http://localhost:5000/example/${folder}/${file}.html`;
+        return expect(exquisite.test({ input, output, url: URL, delay: DELAY })).to.eventually.be.true;
+    });
+}
+
+// Return an array reference objects, a reference object has 2 fields, the folder and the reference itself.
+function _getReferences(referencesFolder) {
+    const folders = fs.readdirSync(path.join(__dirname, `/${referencesFolder}`)).filter(folder => folder !== '.gitignore');
+
+    return folders.map(folder => {
+        return fs.readdirSync(path.join(__dirname, `/${referencesFolder}/${folder}`))
+            .filter(filename => filename.endsWith('.png') && !filename.endsWith('_out.png'))
+            .map(file => {
+                return {
+                    folder: folder,
+                    file: file
+                };
+            });
+    });
+}
+
+// Transform an array of arrays in a single array
+function _flatten(arrays) {
+    return [].concat.apply([], arrays);
+}
