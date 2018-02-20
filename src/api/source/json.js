11be3de9f5cee89301d2e49a962df8cea23b60e2
Javier Goizueta <jgoizueta@gmail.com>
2018-02-20 15:59:25 +0100

    Use local data server
    The intent is to be able to use the server to fetch data from BigQuery

--- src/api/source/json.js
+++ src/api/source/json.js
@@ -19,22 +19,36 @@ export default class JSON {
                 });
             });
         } else if (!this.dataInit) {
-            const geometry = new Float32Array([0.25, 0.25, 0.75, 0.75, 0, 0]);
-            const properties = [];
-            const dataframe = new R.Dataframe(
-                { x: 0, y: 0 },
-                1,
-                geometry,
-                properties,
-            );
-            dataframe.type = 'point';
-            dataframe.active = true;
-            dataframe.size = 3;
 
-            this._addDataframe(dataframe);
-            this.dataInit = true;
+            fetch('http://localhost:3000/', {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                // mode: 'no-cors'
+            }).then(response => {
+                response.json().then(result => {
+                    console.log("-----")
+                    console.log(result);
+                    const geometry = new Float32Array(result);
+                    const properties = [];
+                    const dataframe = new R.Dataframe(
+                        { x: 0, y: 0 },
+                        1,
+                        geometry,
+                        properties,
+                    );
+                    dataframe.type = 'point';
+                    dataframe.active = true;
+                    dataframe.size = 3;
+                    this._addDataframe(dataframe);
+                    this.dataInit = true;
+                });
+            });
         }
     }
+
 
     free() {
         this._client.free();
