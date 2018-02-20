0ab2fe80568ba41b4dc870eccb04af23f528beec
David Manzanares <dmanzanares@cartodb.com>
2018-02-20 12:22:35 +0100

    WIP

--- src/api/source/json.js
+++ src/api/source/json.js
@@ -1,26 +1,99 @@
 import * as R from '../../core/renderer';
+import * as rsys from '../../client/rsys';
+import Property from '../../core/style/expressions/property';
+
+
+const DEG2RAD = Math.PI / 180;
+const EARTH_RADIUS = 6378137;
+const WM_R = EARTH_RADIUS * Math.PI; // Webmercator *radius*: half length Earth's circumference
+const WM_2R = WM_R * 2; // Webmercator coordinate range (Earth's circumference)
+
+const PROPERTY = 'BillingCity';
+
+// Webmercator projection
+function Wmxy(latLng) {
+    let lat = latLng.lat * DEG2RAD;
+    let lng = latLng.lng * DEG2RAD;
+    let x = lng * EARTH_RADIUS;
+    let y = Math.log(Math.tan(lat / 2 + Math.PI / 4)) * EARTH_RADIUS;
+    return { x: x, y: y };
+}
+
 
 export default class JSON {
-    constructor() {
+    constructor(data) {
+        this.data = data;
+        this._categoryStringToIDMap = {};
+        this._numCategories = 0;
     }
 
     bindLayer(addDataframe, removeDataframe) {
         this._addDataframe = addDataframe;
         this._removeDataframe = removeDataframe;
     }
-
+    _getCategoryIDFromString(category) {
+        if (this._categoryStringToIDMap[category]) {
+            return this._categoryStringToIDMap[category];
+        }
+        this._numCategories++;
+        this._categoryStringToIDMap[category] = this._numCategories;
+        return this._numCategories;
+    }
     requestData(viewport, mns, resolution) {
         if (!this.metadataInit) {
             this.metadataInit = true;
+
+            let names = [];
+            let counts = [];
+            let map = {};
+            const numFeatures = this.data.length;
+            for (let i = 0; i < numFeatures; i++) {
+                const f = this.data[i];
+                const catName = f[PROPERTY];
+                if (map[catName] === undefined) {
+                    map[catName] = names.length;
+                    names.push(catName);
+                    counts.push(1);
+                } else {
+                    const id = map[catName];
+                    counts[id]++;
+                }
+            }
+
+
             return new Promise(resolve => {
-                resolve({
+                let metadata = {
                     featureCount: 1000,
-                    columns: [],
-                });
+                    columns: [
+                        {
+                            name: PROPERTY,
+                            type: 'category',
+                            categoryNames: names,
+                            categoryCounts: counts
+                        }
+                    ],
+                    categoryIDs: {},
+                };
+                names.map(name => metadata.categoryIDs[name] = this._getCategoryIDFromString(name));
+                console.log(metadata);
+                resolve(metadata);
             });
         } else if (!this.dataInit) {
-            const geometry = new Float32Array([0.25, 0.25, 0.75, 0.75, 0, 0]);
-            const properties = [];
+            const numFeatures = this.data.length;
+            console.log(numFeatures);
+            const geometry = new Float32Array(numFeatures * 2);
+            const properties = {};
+            properties[PROPERTY] = new Float32Array(numFeatures + 1024);
+            for (let i = 0; i < numFeatures; i++) {
+                const f = this.data[i];
+                const lat = f.Location__Latitude__s;
+                const lng = f.Location__Longitude__s;
+                const wm = Wmxy({ lat, lng });
+                const r = rsys.wToR(wm.x, wm.y, { scale: WM_R, center: { x: 0, y: 0 } });
+                geometry[2 * i + 0] = r.x;
+                geometry[2 * i + 1] = r.y;
+                properties[PROPERTY][i] = this._getCategoryIDFromString(f[PROPERTY]);
+            }
             const dataframe = new R.Dataframe(
                 { x: 0, y: 0 },
                 1,
@@ -29,8 +102,8 @@ export default class JSON {
             );
             dataframe.type = 'point';
             dataframe.active = true;
-            dataframe.size = 3;
-
+            dataframe.size = numFeatures;
+            console.log(123);
             this._addDataframe(dataframe);
             this.dataInit = true;
         }
