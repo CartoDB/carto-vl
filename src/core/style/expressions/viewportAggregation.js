14905962d2e4bac9326e3847eb23d50557764fc5
David Manzanares <dmanzanares@cartodb.com>
2018-02-27 11:10:36 +0100

    WIP

--- src/core/style/expressions/viewportAggregation.js
+++ src/core/style/expressions/viewportAggregation.js
@@ -1,20 +1,23 @@
 import Expression from './expression';
 import { float } from '../functions';
 
-export const ViewportMax = generateViewportExpression('max');
-export const ViewportMin = generateViewportExpression('min');
-export const ViewportAvg = generateViewportExpression('avg');
-export const ViewportSum = generateViewportExpression('sum');
-export const ViewportCount = generateViewportExpression('count');
+export const ViewportMax = generateAggregattion('max');
+export const ViewportMin = generateAggregattion('min');
+export const ViewportAvg = generateAggregattion('avg');
+export const ViewportSum = generateAggregattion('sum');
+export const ViewportCount = generateAggregattion('count');
 
-export const GlobalMax = generateViewportExpression('max', true);
-export const GlobalMin = generateViewportExpression('min', true);
-export const GlobalAvg = generateViewportExpression('avg', true);
-export const GlobalSum = generateViewportExpression('sum', true);
-export const GlobalCount = generateViewportExpression('count', true);
+export const GlobalMax = generateAggregattion('max', true);
+export const GlobalMin = generateAggregattion('min', true);
+export const GlobalAvg = generateAggregattion('avg', true);
+export const GlobalSum = generateAggregattion('sum', true);
+export const GlobalCount = generateAggregattion('count', true);
 
-function generateViewportExpression(metadataPropertyName, global) {
-    return class ViewportAggregattion extends Expression {
+export const ViewportPercentile = generatePercentile();
+export const GlobalPercentile = generatePercentile(true);
+
+function generateAggregattion(metadataPropertyName, global) {
+    return class Aggregattion extends Expression {
         /**
          * @jsapi
          * @param {*} property
@@ -29,7 +32,7 @@ function generateViewportExpression(metadataPropertyName, global) {
             this.property._compile(metadata);
             this.type = 'float';
             super.inlineMaker = inline => inline.value;
-            if (global){
+            if (global) {
                 this.value.expr = metadata.columns.find(c => c.name == this.property.name)[metadataPropertyName];
             }
         }
@@ -55,50 +58,59 @@ function generateViewportExpression(metadataPropertyName, global) {
     };
 }
 
-
-export class ViewportPercentile extends Expression {
-    /**
-     * @jsapi
-     * @param {*} property
-     */
-    constructor(property, percentile) {
-        if (!Number.isFinite(percentile)) {
-            throw new Error('Percentile must be a fixed literal number');
+function generatePercentile(global) {
+    return class Percentile extends Expression {
+        /**
+         * @jsapi
+         * @param {*} property
+         */
+        constructor(property, percentile) {
+            if (!Number.isFinite(percentile)) {
+                throw new Error('Percentile must be a fixed literal number');
+            }
+            super({ value: float(0) });
+            this.property = property;
+            this.percentile = percentile;
         }
-        super({ value: float(0) });
-        this.property = property;
-        this.percentile = percentile;
-    }
-    _compile(metadata) {
-        super._compile(metadata);
-        this.property._compile(metadata);
-        this.type = 'float';
-        super.inlineMaker = inline => inline.value;
-    }
-    _getMinimumNeededSchema() {
-        return this.property._getMinimumNeededSchema();
-    }
-    _getDrawMetadataRequirements() {
-        return { columns: [this.property.name] };
-    }
-    _preDraw(drawMetadata, gl) {
-        const column = drawMetadata.columns.find(c => c.name == this.property.name);
-        const total = column.accumHistogram[999];
-        // TODO OPT: this could be faster with binary search
-        for (var i = 0; i < column.histogramBuckets; i++) {
-            if (column.accumHistogram[i] >= this.percentile / 100 * total) {
-                break;
+        _compile(metadata) {
+            super._compile(metadata);
+            this.property._compile(metadata);
+            this.type = 'float';
+            super.inlineMaker = inline => inline.value;
+            if (global) {
+                const copy = metadata.sample.map(s => s[this.input.name]);
+                copy.sort((x, y) => x - y);
+                const p = this.percentile / 100;
+                this.value.expr = copy[Math.floor(p * copy.length)];
             }
         }
-        const br = i / column.histogramBuckets * (column.max - column.min) + column.min;
-        this.value.expr = br;
-
-        if (Math.random() > 0.99) {
-            console.log(`percentile${this.percentile}`, this.property.name, br, drawMetadata);
+        _getMinimumNeededSchema() {
+            return this.property._getMinimumNeededSchema();
         }
-        this.value._preDraw(drawMetadata, gl);
-    }
-    getValue() {
-        return this.value.expr;
-    }
+        _getDrawMetadataRequirements() {
+            return { columns: [this.property.name] };
+        }
+        _preDraw(drawMetadata, gl) {
+            if (!global) {
+                const column = drawMetadata.columns.find(c => c.name == this.property.name);
+                const total = column.accumHistogram[999];
+                // TODO OPT: this could be faster with binary search
+                for (var i = 0; i < column.histogramBuckets; i++) {
+                    if (column.accumHistogram[i] >= this.percentile / 100 * total) {
+                        break;
+                    }
+                }
+                const br = i / column.histogramBuckets * (column.max - column.min) + column.min;
+                this.value.expr = br;
+            }
+
+            if (Math.random() > 0.99) {
+                console.log(`percentile${this.percentile}`, this.property.name, this.value.expr);
+            }
+            this.value._preDraw(drawMetadata, gl);
+        }
+        getValue() {
+            return this.value.expr;
+        }
+    };
 }
