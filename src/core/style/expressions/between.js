3d028f1c7757897f8ecd45d1648da8919eb6a28b
David Manzanares <dmanzanares@cartodb.com>
2018-03-08 11:04:52 +0100

    Add docs for between()

--- src/core/style/expressions/between.js
+++ src/core/style/expressions/between.js
@@ -1,7 +1,25 @@
 import Expression from './expression';
 
-/** 
- * Check if a given value belongs to an inclusive range (including limits).
+/**
+ *
+ * Check if a given value is contained within an inclusive range (including the limits).
+ *
+ * @param {carto.style.expressions.expression | number} value - numeric expression that is going to be tested against the [lowerLimit, upperLimit] range
+ * @param {carto.style.expressions.expression | number} lowerLimit - numeric expression with the lower limit of the range
+ * @param {carto.style.expressions.expression | number} upperLimit -  numeric expression with the upper limit of the range
+ * @return {carto.style.expressions.expression} numeric expression with the result of the check: 1 if value is inside the range, 0 otherwise
+ *
+ * @example <caption>Display only cities where the population density is within the [50,100] range.</caption>
+ * const s = carto.style.expressions;
+ * const $dn = s.property('populationDensity');
+ * const style = new carto.Style({
+ *  filter: s.between($dn, 50, 100);
+ * });
+ *
+ * @memberof carto.style.expressions
+ * @name in
+ * @function
+ * @api
  */
 export default class Between extends Expression {
     constructor(value, lowerLimit, upperLimit) {
