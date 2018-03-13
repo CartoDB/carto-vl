3013599071c2be9583e8bf36fab3189e357be97d
David M <dmanzanares@cartodb.com>
2018-03-13 18:02:35 +0100

    Improve error control for floatConstant

--- test/unit/core/style/expressions/floatConstant.test.js
+++ test/unit/core/style/expressions/floatConstant.test.js
@@ -1,6 +1,21 @@
 import * as s from '../../../../../src/core/style/functions';
 
 describe('src/core/style/expressions/floatConstant', () => {
+    describe('error control', () => {
+        it('floatConstant of undefined should throw', () => {
+            expect(() => s.floatConstant(undefined)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*x[\s\S]*/g);
+        });
+        it('floatConstant of strings should throw', () => {
+            expect(() => s.floatConstant('123')).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*x[\s\S]*/g);
+        });
+    });
+
+    describe('compiled type', () => {
+        it('floatConstant(100) should be of type float', () => {
+            expect(s.floatConstant(100).type).toEqual('float');
+        });
+    });
+
     describe('.eval()', () => {
         it('should return the float value', () => {
             const actual = s.floatConstant(101).eval();
