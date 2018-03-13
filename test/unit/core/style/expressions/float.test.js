21b22bb7e9a914a48eace47014d21d4419762c38
David M <dmanzanares@cartodb.com>
2018-03-13 18:01:16 +0100

    Improve float() error control

--- test/unit/core/style/expressions/float.test.js
+++ test/unit/core/style/expressions/float.test.js
@@ -1,7 +1,22 @@
 import * as s from '../../../../../src/core/style/functions';
 
 describe('src/core/style/expressions/float', () => {
-    describe('.eval()', () => {
+    describe('error control', () => {
+        it('float of undefined should throw', () => {
+            expect(() => s.float(undefined)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*x[\s\S]*/g);
+        });
+        it('float of strings should throw', () => {
+            expect(() => s.float('123')).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*x[\s\S]*/g);
+        });
+    });
+
+    describe('compiled type', ()=>{
+        it('float(100) should be of type float', () => {
+            expect(s.float(100).type).toEqual('float');
+        });
+    });
+
+    describe('eval', () => {
         it('should return the float value', () => {
             const actual = s.float(101).eval();
 
