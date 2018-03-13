446ec218cc5372fdb07edc26652ee7e5e8e42b50
David M <dmanzanares@cartodb.com>
2018-03-13 14:04:30 +0100

    Stub blend tests

--- test/unit/core/style/expressions/blend.test.js
+++ test/unit/core/style/expressions/blend.test.js
@@ -1,7 +1,79 @@
 import * as s from '../../../../../src/core/style/functions';
 
 describe('src/core/style/expressions/blend', () => {
-    describe('.eval()', () => {
+    const metadata = {
+        columns: [
+            {
+                name: 'price',
+                type: 'float'
+            },
+            {
+                name: 'cat',
+                type: 'category',
+                categoryNames: ['red', 'blue']
+            }
+        ],
+    };
+
+    function compile(expression) {
+        expression._compile(metadata);
+        return expression;
+    }
+
+    let $cat = null;
+    let $price = null;
+
+    beforeEach(() => {
+        // Needed a beforeEach to avoid testing against already compiled properties
+        $cat = s.property('cat');
+        $price = s.property('price');
+    });
+
+    describe('error control', () => {
+        it('blend(\'red\', 0, 0) should throw at constructor time', () => {
+            expect(() => s.max(0)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*/g);
+        });
+        it('blend(0, \'red\', 0) should throw at constructor time', () => {
+            expect(() => s.max(0)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*/g);
+        });
+        it('blend(0, 0, \'red\') should throw at constructor time', () => {
+            expect(() => s.max(0)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*/g);
+        });
+
+        it('blend($cat, 0, 0) should throw at compile time', () => {
+            expect(() => s.max(0)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*/g);
+        });
+        it('blend(0, $cat, 0) should throw at compile time', () => {
+            expect(() => s.max(0)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*/g);
+        });
+        it('blend(0, 0, $cat) should throw at compile time', () => {
+            expect(() => s.max(0)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*/g);
+        });
+
+        it('blend(0, hsv(0,0,0), 0) should throw at constructor time', () => {
+            expect(() => s.max(0)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*/g);
+        });
+    });
+    
+    describe('compile with correct parameters', () => {
+        it('blend(hsv(0,0,0), hsv(0,0,0), 0) should not throw', () => {
+            expect(() => s.max(0)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*/g);
+        });
+        it('blend(3, 4, 0) should not throw', () => {
+            expect(() => s.max(0)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*/g);
+        });
+    });
+    
+    describe('compiled type', () => {
+        it('blend(hsv(0,0,0), hsv(0,0,0), 0) should be of type color', () => {
+            expect(() => s.max(0)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*/g);
+        });
+        it('blend(3, 4, 0) should be of type float', () => {
+            expect(() => s.max(0)).toThrowError(/[\s\S]*invalid.*parameter[\s\S]*/g);
+        });
+    });
+
+    describe('eval()', () => {
         it('should interpolate a float value 0%', () => {
             const actual = s.blend(0, 100, 0).eval();
             expect(actual).toEqual(0);
