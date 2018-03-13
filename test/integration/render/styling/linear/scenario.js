2f9db518d3b31e15c47b22aade3898e3275347cf
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-03-13 17:18:43 +0100

    Update render/styling/linear test

--- test/integration/render/styling/linear/scenario.js
+++ test/integration/render/styling/linear/scenario.js
@@ -1,11 +1,9 @@
-/* global carto geojson */
-
 const map = new carto.Map({
     container: 'map',
     background: 'black'
 });
 
-const source = new carto.source.GeoJSON(geojson);
+const source = new carto.source.GeoJSON(sources['points']);
 const style = new carto.Style(`
     width: 20,
     color: hsv(0, linear($numeric, 0, 10), 1)
