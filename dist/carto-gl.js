f75c2dfaf397b9edabd9836e566787d900eb16f1
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-03-13 18:13:47 +0100

    Auto generated gh-pages

--- dist/carto-gl.js
+++ dist/carto-gl.js
@@ -6925,7 +6925,7 @@ class Linear extends __WEBPACK_IMPORTED_MODULE_0__expression__["a" /* default */
             this.inlineMaker = (inline) => `((${inline.input}-${smin.toFixed(20)})/(${(smax - smin).toFixed(20)}))`;
 
         } else {
-            this.inlineMaker = (inline) => `((${inline.input}-${this.min.value.expr})/(${inline.max}-${inline.min}))`;
+            this.inlineMaker = (inline) => `((${inline.input}-${inline.min})/(${inline.max}-${inline.min}))`;
         }
     }
     eval(feature) {
@@ -7995,6 +7995,7 @@ class NoOrder extends __WEBPACK_IMPORTED_MODULE_0__expression__["a" /* default *
 
 
 
+const SAMPLE_TARGET_SIZE = 1000;
 
 class GeoJSON extends __WEBPACK_IMPORTED_MODULE_0__base__["a" /* default */] {
 
@@ -8028,7 +8029,12 @@ class GeoJSON extends __WEBPACK_IMPORTED_MODULE_0__base__["a" /* default */] {
 
         this._type = ''; // Point, LineString, MultiLineString, Polygon, MultiPolygon
         this._status = 'init'; // init -> metadata -> data
+        this._categoryStringToIDMap = {};
+        this._numCategories = 0;
+        this._numFields = [];
+        this._catFields = [];
         this._features = this._getFeatures(data);
+        this._metadata = this._computeMetadata();
     }
 
     bindLayer(addDataframe, removeDataframe) {
@@ -8069,15 +8075,12 @@ class GeoJSON extends __WEBPACK_IMPORTED_MODULE_0__base__["a" /* default */] {
     }
 
     _requestMetadata() {
-        return new Promise(resolve => {
-            let metadata = {};
-            resolve(metadata);
-        });
+        return Promise.resolve(this._metadata);
     }
 
     _requestData() {
         const geometry = this._decodeGeometry();
-        const properties = {};
+        const properties = this._decodeProperties();
         const dataframe = new __WEBPACK_IMPORTED_MODULE_1__core_renderer__["a" /* Dataframe */](
             { x: 0, y: 0 },
             1,
@@ -8088,6 +8091,120 @@ class GeoJSON extends __WEBPACK_IMPORTED_MODULE_0__base__["a" /* default */] {
         dataframe.active = true;
         dataframe.size = this._features.length;
         this._addDataframe(dataframe);
+    }
+
+    _computeMetadata() {
+        const metadata = {
+            columns: [],
+            categoryIDs: {},
+            sample: [],
+            featureCount: this._features.length,
+        };
+        for (var i = 0; i < this._features.length; i++) {
+            const properties = this._features[i].properties;
+            Object.keys(properties).map(name => {
+                const value = properties[name];
+                const type = Number.isFinite(value) ? 'float' : 'category';
+                if (type == 'float') {
+                    this._addNumericPropertyToMetadata(name, value, metadata);
+                } else {
+                    this._addCategoryPropertyToMetadata(name, value, metadata);
+                }
+            });
+            this._sampleFeatureOnMetadata(properties, metadata, this._features.length);
+        }
+        this._numFields.map(name => {
+            const column = metadata.columns.find(c => c.name == name);
+            column.avg = column.sum / column.count;
+        });
+        this._catFields.map(name => {
+            const column = metadata.columns.find(c => c.name == name);
+            column.categoryNames.map(name => metadata.categoryIDs[name] = this._getCategoryIDFromString(name));
+        });
+        this._metadata = metadata;
+        return metadata;
+    }
+
+    _sampleFeatureOnMetadata(feature, metadata, featureCount) {
+        if (featureCount > SAMPLE_TARGET_SIZE) {
+            const sampling = SAMPLE_TARGET_SIZE / featureCount;
+            if (Math.random() > sampling) {
+                return;
+            }
+        }
+        metadata.sample.push(feature);
+    }
+
+    _addNumericPropertyToMetadata(propertyName, value, metadata) {
+        if (this._catFields.includes(propertyName)) {
+            throw new Error(`Unsupported GeoJSON: the property '${propertyName}' has different types in different features.`);
+        }
+        if (!this._numFields.includes(propertyName)) {
+            this._numFields.push(propertyName);
+            metadata.columns.push({
+                name: propertyName,
+                type: 'float',
+                min: Number.POSITIVE_INFINITY,
+                max: Number.NEGATIVE_INFINITY,
+                avg: Number.NaN,
+                sum: 0,
+                count: 0
+            });
+        }
+        const column = metadata.columns.find(c => c.name == propertyName);
+        column.min = Math.min(column.min, value);
+        column.max = Math.max(column.max, value);
+        column.sum += value;
+        column.count++;
+    }
+    _addCategoryPropertyToMetadata(propertyName, value, metadata) {
+        if (this._numFields.includes(propertyName)) {
+            throw new Error(`Unsupported GeoJSON: the property '${propertyName}' has different types in different features.`);
+        }
+        if (!this._catFields.includes(propertyName)) {
+            this._catFields.push(propertyName);
+            metadata.columns.push({
+                name: propertyName,
+                type: 'category',
+                categoryNames: [],
+                categoryCounts: [],
+            });
+        }
+        const column = metadata.columns.find(c => c.name == propertyName);
+        if (!column.categoryNames.includes(value)) {
+            column.categoryNames.push(value);
+            column.categoryCounts.push(0);
+        }
+        column.categoryCounts[column.categoryNames.indexOf(value)]++;
+    }
+
+    _decodeProperties() {
+        const properties = {};
+        this._numFields.concat(this._catFields).map(name => {
+            // The dataframe expects to have a padding of 1024, adding 1024 empty values assures this condition is met            
+            properties[name] = new Float32Array(this._features.length + 1024);
+        });
+        for (var i = 0; i < this._features.length; i++) {
+            const f = this._features[i];
+
+            this._catFields.map(name => {
+                properties[name][i] = this._getCategoryIDFromString(f.properties[name]);
+            });
+            this._numFields.map(name => {
+                properties[name][i] = Number(f.properties[name]);
+            });
+            // TODO support date / timestamp properties
+        }
+        return properties;
+    }
+
+    _getCategoryIDFromString(category) {
+        if (this._categoryStringToIDMap[category] !== undefined) {
+            return this._categoryStringToIDMap[category];
+        }
+        this._categoryStringToIDMap[category] = this._numCategories;
+        this._numCategories++;
+        return this._categoryStringToIDMap[category];
     }
 
     _getDataframeType(type) {
