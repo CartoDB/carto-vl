2e40287f74b32dd5bf663c9cd744364d0dee1820
David Manzanares <dmanzanares@cartodb.com>
2018-03-14 17:27:47 +0100

    Avoid unnecessary SQL calls to the server

--- src/client/windshaft.js
+++ src/client/windshaft.js
@@ -554,6 +554,9 @@ export default class Windshaft {
     }
 
     async getDatesTypes(names, query, conf) {
+        if (names.length == 0) {
+            return [];
+        }
         const aggFns = ['min', 'max'];
         const datesSelect = names.map(name =>
             aggFns.map(fn => `${fn}(${name}) AS ${name}_${fn}`)
@@ -613,7 +616,7 @@ function getAggFN(name) {
     return s.substr(0, s.indexOf('_'));
 }
 
-const endpoint = (conf, path='') => {
+const endpoint = (conf, path = '') => {
     let url = `${conf.mapsServerURL}/api/v1/map`;
     if (path) {
         url += '/' + path;
