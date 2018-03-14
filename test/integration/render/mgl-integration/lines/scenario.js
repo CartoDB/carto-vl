5917c1a7179075c0e61f57640e9f197d30b0190f
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-03-14 13:24:23 +0100

    Add more mgl-integration tests

---
+++ test/integration/render/mgl-integration/lines/scenario.js
@@ -0,0 +1,14 @@
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: { version: 8, sources: {}, layers: [] },
+    center: [0, 0],
+    zoom: 0
+});
+
+const source = new carto.source.GeoJSON(sources['line-string']);
+const style = new carto.Style(`
+    width: 20
+`);
+const layer = new carto.Layer('layer', source, style);
+
+layer.addTo(map);
