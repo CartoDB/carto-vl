d9c12090c8a414b19ebe63d47c559dac92b481e3
David Manzanares <dmanzanares@cartodb.com>
2018-03-19 15:48:07 +0100

    Minor code style change

--- test/unit/core/style/parser.test.js
+++ test/unit/core/style/parser.test.js
@@ -4,14 +4,13 @@ describe('src/core/style/parser', () => {
     describe('and/or don\'t interfere with `ordering`', () => {
         it('`color: rgba(1,1,1,1) ordering: asc(width())` should not throw', () => {
             const str = 'color: rgba(1,1,1,1)\nordering: asc(width())';
-            expect(()=>parseStyleDefinition(str)).not.toThrow();
+            expect(() => parseStyleDefinition(str)).not.toThrow();
         });
     });
     describe('and/or are accepted', () => {
         it('`color: rgba(1,1,1,1)\nfilter: 1 or 0` should not throw', () => {
             const str = 'color: rgba(1,1,1,1)\nfilter: 1 or 0';
-            expect(()=>parseStyleDefinition(str)).not.toThrow();
+            expect(() => parseStyleDefinition(str)).not.toThrow();
         });
     });
 });
-
