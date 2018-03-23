ae78f1edb6c422c49558582837e738a3f524190a
David Manzanares <dmanzanares@cartodb.com>
2018-03-23 13:34:19 +0100

    Add simple dataframe point in polygon tests

--- test/unit/core/dataframe.test.js
+++ test/unit/core/dataframe.test.js
@@ -1,3 +1,5 @@
+import Dataframe from '../../../src/core/dataframe';
+
 // import dataframe from '../../../src/core/dataframe';
 
 describe('src/core/dataframe', () => {
@@ -13,8 +15,42 @@ describe('src/core/dataframe', () => {
         });
 
         describe('when dataframe is polygon type', () => {
-            it('should return an empty list when there are no lines at the given position', () => { });
-            it('should return a list containing the features at the given position', () => { });
+            const polygon1 = {
+                flat: [
+                    0, 0,
+                    0, 1,
+                    1, 0
+                ],
+                holes: [],
+            };
+            const df = new Dataframe({
+                center: { x: 0, y: 0 },
+                scale: 1,
+                geom: [[polygon1]],
+                properties: {
+                    id: [0]
+                },
+                type: 'polygon',
+                size: 1,
+                active: true,
+            });
+            const feature1 = {
+                properties: {
+                    id: 0
+                }
+            };
+            it('should return an empty list when there are no features at the given position', () => {
+                expect(df.getFeaturesAtPosition({ x: -0.01, y: 0.0 })).toEqual([]);
+                expect(df.getFeaturesAtPosition({ x: 0.51, y: 0.51 })).toEqual([]);
+                expect(df.getFeaturesAtPosition({ x: 0.0, y: 1.01 })).toEqual([]);
+                expect(df.getFeaturesAtPosition({ x: 1.01, y: 0.0 })).toEqual([]);
+            });
+            it('should return a list containing the features at the given position', () => {
+                expect(df.getFeaturesAtPosition({ x: 0.0, y: 0.0 })).toEqual([feature1]);
+                expect(df.getFeaturesAtPosition({ x: 0.5, y: 0.5 })).toEqual([feature1]);
+                expect(df.getFeaturesAtPosition({ x: 0.0, y: 1.0 })).toEqual([feature1]);
+                expect(df.getFeaturesAtPosition({ x: 1.0, y: 0.0 })).toEqual([feature1]);
+            });
         });
 
     });
