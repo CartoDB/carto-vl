fd923ac5167250871c6591e91e405bbaf1db97aa
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-03-28 14:10:27 +0200

    Add render tests for RGB/RGBA expressions

---
+++ test/integration/render/scenarios/color/rgba/scenario.js
@@ -0,0 +1,10 @@
+const map = new carto.Map({
+    container: 'map',
+    background: 'black'
+});
+
+const source = new carto.source.GeoJSON(sources['line-string']);
+const style = new carto.Style('color: rgba(255, 0, 0, 0.5)');
+const layer = new carto.Layer('layer', source, style);
+
+layer.addTo(map);
