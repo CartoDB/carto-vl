7fa9a0d1ee9c9c845a7f7bdf29e5c4e5df0e66bb
David M <dmanzanares@cartodb.com>
2018-04-04 12:44:24 +0200

    Add MGL shared sources test

--- test/integration/render/scenarios/mgl-integration/shared-sources/scenario.js
+++ test/integration/render/scenarios/mgl-integration/shared-sources/scenario.js
@@ -1,6 +1,8 @@
-const map = new carto.Map({
+const map = new mapboxgl.Map({
     container: 'map',
-    background: 'black'
+    style: { version: 8, sources: {}, layers: [] },
+    center: [0, 0],
+    zoom: 0
 });
 
 const points = new carto.source.GeoJSON(sources['points-with-5-properties']);
