27458abda3b1028db55d4f90e8fe74c70e153d71
David Manzanares <dmanzanares@cartodb.com>
2018-04-09 14:52:52 +0200

    Fix E2E tests

---
+++ test/acceptance/e2e/pop_density_points/scenario.js
@@ -0,0 +1,25 @@
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
+    center: [-20, 33],
+    zoom: 3
+});
+
+carto.setDefaultAuth({
+    user: 'cartogl',
+    apiKey: 'YOUR_API_KEY'
+});
+
+const source = new carto.source.Dataset('pop_density_points');
+const s = carto.style.expressions;
+const $dn = s.property('dn');
+const style = new carto.Style({
+    width: 4 * s.zoom(),
+    color: s.ramp(s.linear($dn, 1, 300), s.palettes.prism),
+    strokeColor: s.rgba(0, 0, 0, 0.2),
+    strokeWidth: 1,
+    filter: s.between($dn, 100, 140)
+});
+const layer = new carto.Layer('myCartoLayer', source, style);
+
+layer.addTo(map);
