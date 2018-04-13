5b41494eb39a4a352082cbbfac67f943b1452ecb
David Manzanares <dmanzanares@cartodb.com>
2018-04-13 16:29:56 +0200

    Fix render tests

--- test/integration/render/scenarios/variables/simple/scenario.js
+++ test/integration/render/scenarios/variables/simple/scenario.js
@@ -5,10 +5,9 @@ const map = new carto.Map({
 
 const source = new carto.source.GeoJSON(sources['points']);
 // Check that precision is good
-const style = new carto.Style(`width: ramp(linear($numeric, 0, 10), [0.10,0.20,0.30]) * $ten * $ten
-                               variables: [
-                                    ten: 3+7  
-                               ]`);
+const style = new carto.Style(`width: ramp(linear($numeric, 0, 10), [0.10,0.20,0.30]) * __cartovl_variable_oneHundred
+                                __cartovl_variable_oneHundred: 10*10
+                               `);
 const layer = new carto.Layer('layer', source, style);
 
 layer.addTo(map);
