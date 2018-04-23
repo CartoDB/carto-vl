716eac7cebc169caeb5d3862958ea010b8ea5171
IagoLast <iago.lastra@gmail.com>
2018-04-23 14:28:11 +0200

    Fix test hierarchy

--- test/integration/user/api/interactivity.test.js
+++ test/integration/user/api/interactivity.test.js
@@ -113,183 +113,145 @@ describe('Interactivity', () => {
                 });
             });
         });
+    });
 
-        describe('when the user move the mouse on the map', () => {
-            let interactivity;
+    describe('when the user move the mouse on the map', () => {
+        let interactivity;
 
-            describe('and the mouse enters in a feature', () => {
-                it('should fire a featureHover event with a features list containing the entered feature', done => {
-                    interactivity = new carto.Interactivity(layer);
-                    interactivity.on('featureHover', event => {
-                        expect(event.features[0].id).toEqual(0);
-                        expect(event.features[0].layerId).toEqual('layer');
-                        done();
-                    });
-                    layer.on('loaded', () => {
-                        // Move mouse inside a feature
-                        map.fire('click', { lngLat: { lng: -10, lat: -10 } });
-                        map.fire('mousemove', { lngLat: { lng: 10, lat: 10 } });
-                    });
+        describe('and the mouse enters in a feature', () => {
+            it('should fire a featureHover event with a features list containing the entered feature', done => {
+                interactivity = new carto.Interactivity(layer);
+                interactivity.on('featureHover', event => {
+                    expect(event.features[0].id).toEqual(0);
+                    expect(event.features[0].layerId).toEqual('layer');
+                    done();
                 });
-
-                it('should fire a featureEnter event with a features list containing the entered feature', done => {
-                    interactivity = new carto.Interactivity(layer);
-                    interactivity.on('featureEnter', event => {
-                        expect(event.features[0].id).toEqual(0);
-                        expect(event.features[0].layerId).toEqual('layer');
-                        done();
-                    });
-                    layer.on('loaded', () => {
-                        // Move mouse inside a feature
-                        map.fire('mousemove', { lngLat: { lng: 10, lat: 10 } });
-                    });
-                });
-
-                it('should not fire a featureEnter event when the mouse is moved inside the same feature', done => {
-                    interactivity = new carto.Interactivity(layer);
-                    const featureClickOutSpy = jasmine.createSpy('featureClickOutSpy');
-                    layer.on('loaded', () => {
-                        // Move mouse inside a feature
-                        map.fire('mousemove', { lngLat: { lng: 10, lat: 10 } });
-                        interactivity.on('featureEnter', featureClickOutSpy);
-                        interactivity.on('featureHover', () => {
-                            expect(featureClickOutSpy).not.toHaveBeenCalled();
-                            done();
-                        });
-                        // Move mouse inside the same feature
-                        map.fire('mousemove', { lngLat: { lng: 20, lat: 20 } });
-                    });
+                layer.on('loaded', () => {
+                    // Move mouse inside a feature
+                    map.fire('click', { lngLat: { lng: -10, lat: -10 } });
+                    map.fire('mousemove', { lngLat: { lng: 10, lat: 10 } });
                 });
             });
 
-            describe('and the mouse leaves a feature', () => {
-                it('should fire a featureHover event with an empty features list', done => {
-                    interactivity = new carto.Interactivity(layer);
-                    layer.on('loaded', () => {
-                        // Move mouse inside a feature
-                        map.fire('mousemove', { lngLat: { lng: 10, lat: 10 } });
-                        interactivity.on('featureHover', event => {
-                            expect(event.features.length).toEqual(0);
-                            done();
-                        });
-                        // Move mouse outside any feature
-                        map.fire('mousemove', { lngLat: { lng: -10, lat: -10 } });
-                    });
+            it('should fire a featureEnter event with a features list containing the entered feature', done => {
+                interactivity = new carto.Interactivity(layer);
+                interactivity.on('featureEnter', event => {
+                    expect(event.features[0].id).toEqual(0);
+                    expect(event.features[0].layerId).toEqual('layer');
+                    done();
                 });
-
-                it('should fire a featureLeave event with a features list containing the previously entered feature', done => {
-                    interactivity = new carto.Interactivity(layer);
-                    layer.on('loaded', () => {
-                        // Move mouse inside a feature
-                        map.fire('mousemove', { lngLat: { lng: 10, lat: 10 } });
-                        interactivity.on('featureLeave', event => {
-                            expect(event.features[0].id).toEqual(0);
-                            expect(event.features[0].layerId).toEqual('layer');
-                            done();
-                        });
-                        // Move mouse outside the feature
-                        map.fire('mousemove', { lngLat: { lng: -10, lat: -10 } });
-                    });
+                layer.on('loaded', () => {
+                    // Move mouse inside a feature
+                    map.fire('mousemove', { lngLat: { lng: 10, lat: 10 } });
                 });
+            });
 
-                it('should not fire a featureLeave event when the mouse is moved outside any feature', done => {
-                    interactivity = new carto.Interactivity(layer);
-                    layer.on('loaded', () => {
-                        const featureLeaveSpy = jasmine.createSpy('featureLeaveSpy');
-                        // Move mouse outside any feature
-                        map.fire('mousemove', { lngLat: { lng: -10, lat: -10 } });
-                        interactivity.on('featureLeave', featureLeaveSpy);
-                        interactivity.on('featureHover', () => {
-                            expect(featureLeaveSpy).not.toHaveBeenCalled();
-                            done();
-                        });
-                        // Move mouse outside any feature
-                        map.fire('mousemove', { lngLat: { lng: -20, lat: -20 } });
+            it('should not fire a featureEnter event when the mouse is moved inside the same feature', done => {
+                interactivity = new carto.Interactivity(layer);
+                const featureClickOutSpy = jasmine.createSpy('featureClickOutSpy');
+                layer.on('loaded', () => {
+                    // Move mouse inside a feature
+                    map.fire('mousemove', { lngLat: { lng: 10, lat: 10 } });
+                    interactivity.on('featureEnter', featureClickOutSpy);
+                    interactivity.on('featureHover', () => {
+                        expect(featureClickOutSpy).not.toHaveBeenCalled();
+                        done();
                     });
+                    // Move mouse inside the same feature
+                    map.fire('mousemove', { lngLat: { lng: 20, lat: 20 } });
                 });
             });
         });
 
-        afterEach(() => {
-            document.body.removeChild(div);
+        describe('and the mouse leaves a feature', () => {
+            it('should fire a featureHover event with an empty features list', done => {
+                interactivity = new carto.Interactivity(layer);
+                layer.on('loaded', () => {
+                    // Move mouse inside a feature
+                    map.fire('mousemove', { lngLat: { lng: 10, lat: 10 } });
+                    interactivity.on('featureHover', event => {
+                        expect(event.features.length).toEqual(0);
+                        done();
+                    });
+                    // Move mouse outside any feature
+                    map.fire('mousemove', { lngLat: { lng: -10, lat: -10 } });
+                });
+            });
+
+            it('should fire a featureLeave event with a features list containing the previously entered feature', done => {
+                interactivity = new carto.Interactivity(layer);
+                layer.on('loaded', () => {
+                    // Move mouse inside a feature
+                    map.fire('mousemove', { lngLat: { lng: 10, lat: 10 } });
+                    interactivity.on('featureLeave', event => {
+                        expect(event.features[0].id).toEqual(0);
+                        expect(event.features[0].layerId).toEqual('layer');
+                        done();
+                    });
+                    // Move mouse outside the feature
+                    map.fire('mousemove', { lngLat: { lng: -10, lat: -10 } });
+                });
+            });
+
+            it('should not fire a featureLeave event when the mouse is moved outside any feature', done => {
+                interactivity = new carto.Interactivity(layer);
+                layer.on('loaded', () => {
+                    const featureLeaveSpy = jasmine.createSpy('featureLeaveSpy');
+                    // Move mouse outside any feature
+                    map.fire('mousemove', { lngLat: { lng: -10, lat: -10 } });
+                    interactivity.on('featureLeave', featureLeaveSpy);
+                    interactivity.on('featureHover', () => {
+                        expect(featureLeaveSpy).not.toHaveBeenCalled();
+                        done();
+                    });
+                    // Move mouse outside any feature
+                    map.fire('mousemove', { lngLat: { lng: -20, lat: -20 } });
+                });
+            });
         });
     });
 
-    function _setup(name) {
-        const div = document.createElement('div');
-        div.id = name;
-        div.style.width = '1000px';
-        div.style.height = '1000px';
-        document.body.appendChild(div);
-
-        const map = new mapboxgl.Map({
-            container: name,
-            style: { version: 8, sources: {}, layers: [] },
-            center: [30, 30],
-            zoom: 12
-        });
-
-        return { div, map };
-    }
-
-    function click() {
-        const mglInteractivity = document.querySelector('.mapboxgl-interactive');
-        const ev = new MouseEvent('click', {});
-        mglInteractivity.dispatchEvent(ev);
-    }
-
-    const featureJson = {
-        type: 'Feature',
-        geometry: {
-            type: 'Polygon',
-            coordinates: [
-                [
-                    [0, 0],
-                    [50, 0],
-                    [50, 50],
-                    [0, 50],
-                    [0, 0]
-                ]
-            ]
-        },
-        properties: {}
-    };
-
-    const featureCollectionJson = {
-        type: 'FeatureCollection',
-        features: [
-            {
-                type: 'Feature',
-                geometry: {
-                    type: 'Polygon',
-                    coordinates: [
-                        [
-                            [0, 0],
-                            [50, 0],
-                            [50, 50],
-                            [0, 50],
-                            [0, 0]
-                        ]
-                    ]
-                },
-                properties: {}
-            },
-            {
-                type: 'Feature',
-                geometry: {
-                    type: 'Polygon',
-                    coordinates: [
-                        [
-                            [0, 0],
-                            [55, 0],
-                            [55, 55],
-                            [0, 55],
-                            [0, 0]
-                        ]
-                    ]
-                },
-                properties: {}
-            }
-        ]
-    };
+    afterEach(() => {
+        document.body.removeChild(div);
+    });
 });
+
+function _setup(name) {
+    const div = document.createElement('div');
+    div.id = name;
+    div.style.width = '1000px';
+    div.style.height = '1000px';
+    document.body.appendChild(div);
+
+    const map = new mapboxgl.Map({
+        container: name,
+        style: { version: 8, sources: {}, layers: [] },
+        center: [30, 30],
+        zoom: 12
+    });
+
+    return { div, map };
+}
+
+function click() {
+    const mglInteractivity = document.querySelector('.mapboxgl-interactive');
+    const ev = new MouseEvent('click', {});
+    mglInteractivity.dispatchEvent(ev);
+}
+
+const featureJson = {
+    type: 'Feature',
+    geometry: {
+        type: 'Polygon',
+        coordinates: [
+            [
+                [0, 0],
+                [50, 0],
+                [50, 50],
+                [0, 50],
+                [0, 0]
+            ]
+        ]
+    },
+    properties: {}
+};
