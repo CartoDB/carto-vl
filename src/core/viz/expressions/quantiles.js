fede37a38f55535cb8158a232136bd41c3221352
David Manzanares <dmanzanares@cartodb.com>
2018-05-11 17:17:47 +0200

    Classifier refactor

--- src/core/viz/expressions/quantiles.js
+++ src/core/viz/expressions/quantiles.js
@@ -6,6 +6,66 @@ import * as schema from '../../schema';
 
 let quantilesUID = 0;
 
+
+class Classifier extends BaseExpression {
+    constructor(children, buckets) {
+        let breakpoints = [];
+        for (let i = 0; i < buckets - 1; i++) {
+            children[`arg${i}`] = number(0);
+            breakpoints.push(children[`arg${i}`]);
+        }
+        super(children);
+        this.quantilesUID = quantilesUID++;
+        this.numCategories = buckets;
+        this.buckets = buckets;
+        this.breakpoints = breakpoints;
+        this.type = 'category';
+    }
+    eval(feature) {
+        const input = this.input.eval(feature);
+        const q = this.breakpoints.findIndex(br => input <= br);
+        return q;
+    }
+    _genBreakpoints() {
+    }
+    getBreakpointList() {
+        this._genBreakpoints();
+        return this.breakpoints.map(br => br.expr);
+    }
+    _applyToShaderSource(getGLSLforProperty) {
+        const childSources = this.childrenNames.map(name => this[name]._applyToShaderSource(getGLSLforProperty));
+        let childInlines = {};
+        childSources.map((source, index) => childInlines[this.childrenNames[index]] = source.inline);
+        const funcName = `quantiles${this.quantilesUID}`;
+        const elif = (_, index) =>
+            `${index > 0 ? 'else' : ''} if (x<(${childInlines[`arg${index}`]})){
+        return ${index.toFixed(2)};
+    }`;
+        const funcBody = this.breakpoints.map(elif).join('');
+        const preface = `float ${funcName}(float x){
+    ${funcBody}
+    return ${this.breakpoints.length.toFixed(1)};
+}`;
+        return {
+            preface: this._prefaceCode(childSources.map(s => s.preface).reduce((a, b) => a + b, '') + preface),
+            inline: `${funcName}(${childInlines.input})`
+        };
+    }
+    _preDraw(program, drawMetadata, gl) {
+        this._genBreakpoints();
+        // TODO
+        super._preDraw(program, drawMetadata, gl);
+    }
+    _getColumnName() {
+        if (this.input.aggName) {
+            // Property has aggregation
+            return schema.column.aggColumn(this.input.name, this.input.aggName);
+        }
+        return this.input.name;
+    }
+}
+
+
 /**
  * Generate quantiles of size `n` from the features on the viewport.
  *
@@ -29,8 +89,50 @@ let quantilesUID = 0;
  * @function
  * @api
  */
-export const Quantiles = genQuantiles(false);
+export class Quantiles extends Classifier {
+    constructor(input, buckets) {
+        checkInstance('quantiles', 'input', 0, Property, input && (input.property || input));
+        checkNumber('quantiles', 'buckets', 1, buckets);
 
+        let children = {
+            input
+        };
+        children._histogram = viewportHistogram(input);
+        super(children, buckets);
+    }
+    _compile(metadata) {
+        super._compile(metadata);
+        checkType('quantiles', 'input', 0, 'number', this.input);
+    }
+    _genBreakpoints() {
+        const hist = this._histogram.eval();
+
+        const histogramBuckets = hist.length;
+        const min = hist[0].x[0];
+        const max = hist[histogramBuckets - 1].x[1];
+
+        let prev = 0;
+        const accumHistogram = hist.map(({ y }) => {
+            prev += y;
+            return prev;
+        });
+
+        let i = 0;
+        const total = accumHistogram[histogramBuckets - 1];
+        let brs = [];
+        // TODO OPT: this could be faster with binary search
+        this.breakpoints.map((breakpoint, index) => {
+            for (i; i < histogramBuckets; i++) {
+                if (accumHistogram[i] > (index + 1) / this.buckets * total) {
+                    break;
+                }
+            }
+            const percentileValue = i / histogramBuckets * (max - min) + min;
+            brs.push(percentileValue);
+            breakpoint.expr = percentileValue;
+        });
+    }
+}
 /**
  * Generate quantiles of size `n` from all the features.
  *
@@ -54,113 +156,20 @@ export const Quantiles = genQuantiles(false);
  * @function
  * @api
  */
-export const GlobalQuantiles = genQuantiles(true);
-
-function genQuantiles(global) {
-    return class Quantiles extends BaseExpression {
-        constructor(input, buckets) {
-            checkInstance('quantiles', 'input', 0, Property, input && (input.property || input));
-            checkNumber('quantiles', 'buckets', 1, buckets);
-
-            let children = {
-                input
-            };
-            if (!global) {
-                children._histogram = viewportHistogram(input);
-            }
-            let breakpoints = [];
-            for (let i = 0; i < buckets - 1; i++) {
-                children[`arg${i}`] = number(i * 10);
-                breakpoints.push(children[`arg${i}`]);
-            }
-            super(children);
-            this.quantilesUID = quantilesUID++;
-            this.numCategories = buckets;
-            this.buckets = buckets;
-            this.breakpoints = breakpoints;
-            this.type = 'category';
-        }
-        eval(feature) {
-            const input = this.input.eval(feature);
-            const q = this.breakpoints.findIndex(br => input <= br);
-            return q;
-        }
-        getBreakpointList() {
-            this._genBreakpoints();
-            return this.breakpoints.map(br => br.expr);
-        }
-        _compile(metadata) {
-            super._compile(metadata);
-            checkType('quantiles', 'input', 0, 'number', this.input);
-            if (global) {
-                const copy = metadata.sample.map(s => s[this.input.name]);
-                copy.sort((x, y) => x - y);
-                this.breakpoints.map((breakpoint, index) => {
-                    const p = (index + 1) / this.buckets;
-                    breakpoint.expr = copy[Math.floor(p * copy.length)];
-                });
-            }
-        }
-        _applyToShaderSource(getGLSLforProperty) {
-            const childSources = this.childrenNames.map(name => this[name]._applyToShaderSource(getGLSLforProperty));
-            let childInlines = {};
-            childSources.map((source, index) => childInlines[this.childrenNames[index]] = source.inline);
-            const funcName = `quantiles${this.quantilesUID}`;
-            const elif = (_, index) =>
-                `${index > 0 ? 'else' : ''} if (x<(${childInlines[`arg${index}`]})){
-            return ${index.toFixed(2)};
-        }`;
-            const funcBody = this.breakpoints.map(elif).join('');
-            const preface = `float ${funcName}(float x){
-        ${funcBody}
-        return ${this.breakpoints.length.toFixed(1)};
-    }`;
-            return {
-                preface: this._prefaceCode(childSources.map(s => s.preface).reduce((a, b) => a + b, '') + preface),
-                inline: `${funcName}(${childInlines.input})`
-            };
-        }
-        _genBreakpoints() {
-            if (!global) {
-                const hist = this._histogram.eval();
-
-                const histogramBuckets = hist.length;
-                const min = hist[0].x[0];
-                const max = hist[histogramBuckets - 1].x[1];
-
-                let prev = 0;
-                const accumHistogram = hist.map(({ y }) => {
-                    prev += y;
-                    return prev;
-                });
-
-
-                let i = 0;
-                const total = accumHistogram[histogramBuckets - 1];
-                let brs = [];
-                // TODO OPT: this could be faster with binary search
-                this.breakpoints.map((breakpoint, index) => {
-                    for (i; i < histogramBuckets; i++) {
-                        if (accumHistogram[i] > (index + 1) / this.buckets * total) {
-                            break;
-                        }
-                    }
-                    const percentileValue = i / histogramBuckets * (max - min) + min;
-                    brs.push(percentileValue);
-                    breakpoint.expr = percentileValue;
-                });
-            }
-        }
-        _preDraw(program, drawMetadata, gl) {
-            this._genBreakpoints();
-            super._preDraw(program, drawMetadata, gl);
-        }
-        _getColumnName() {
-            if (this.input.aggName) {
-                // Property has aggregation
-                return schema.column.aggColumn(this.input.name, this.input.aggName);
-            }
-            return this.input.name;
-        }
-    };
+export class GlobalQuantiles extends Classifier {
+    constructor(input, buckets) {
+        checkInstance('globalQuantiles', 'input', 0, Property, input && (input.property || input));
+        checkNumber('globalQuantiles', 'buckets', 1, buckets);
+        super({ input }, buckets);
+    }
+    _compile(metadata) {
+        super._compile(metadata);
+        checkType('globalQuantiles', 'input', 0, 'number', this.input);
+        const copy = metadata.sample.map(s => s[this.input.name]);
+        copy.sort((x, y) => x - y);
+        this.breakpoints.map((breakpoint, index) => {
+            const p = (index + 1) / this.buckets;
+            breakpoint.expr = copy[Math.floor(p * copy.length)];
+        });
+    }
 }
