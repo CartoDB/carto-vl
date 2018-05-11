0b22b63ae0a1d6a93978f0c6cdb525b4c0b8492e
David Manzanares <dmanzanares@cartodb.com>
2018-05-11 11:43:43 +0200

    Add quantiles render tests

---
+++ test/integration/render/scenarios/classification/viewportQuantiles/scenario.js
@@ -0,0 +1,10 @@
+const map = new carto.Map({
+    container: 'map',
+    background: 'black'
+});
+
+const source = new carto.source.GeoJSON(sources['points']);
+const viz = new carto.Viz('color: ramp(quantiles($numeric, 3), PRISM), width: 50');
+const layer = new carto.Layer('layer', source, viz);
+
+layer.addTo(map);
