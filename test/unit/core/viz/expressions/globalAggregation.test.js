106b0febee761096f5aefde58c61b19b02c4a752
IagoLast <iago.lastra@gmail.com>
2018-05-16 13:06:17 +0200

    Update tests

--- test/unit/core/viz/expressions/globalAggregation.test.js
+++ test/unit/core/viz/expressions/globalAggregation.test.js
@@ -2,7 +2,7 @@ import * as s from '../../../../../src/core/viz/functions';
 
 describe('src/core/viz/expressions/viewportAggregation', () => {
     const $price = s.property('price');
-    describe('global filtering', () => {
+    fdescribe('global filtering', () => {
         const fakeMetadata = {
             columns: [{
                 type: 'number',
@@ -18,31 +18,31 @@ describe('src/core/viz/expressions/viewportAggregation', () => {
         it('globalMin($price) should return the metadata min', () => {
             const globalMin = s.globalMin($price);
             globalMin._compile(fakeMetadata);
-            expect(globalMin.eval()).toEqual(0);
+            expect(globalMin.value).toEqual(0);
         });
 
         it('globalAvg($price) should return the metadata avg', () => {
             const globalAvg = s.globalAvg($price);
             globalAvg._compile(fakeMetadata);
-            expect(globalAvg.eval()).toEqual(1);
+            expect(globalAvg.value).toEqual(1);
         });
 
         it('globalMax($price) should return the metadata max', () => {
             const globalMax = s.globalMax($price);
             globalMax._compile(fakeMetadata);
-            expect(globalMax.eval()).toEqual(2);
+            expect(globalMax.value).toEqual(2);
         });
 
         it('globalSum($price) should return the metadata sum', () => {
             const globalSum = s.globalSum($price);
             globalSum._compile(fakeMetadata);
-            expect(globalSum.eval()).toEqual(3);
+            expect(globalSum.value).toEqual(3);
         });
 
         it('globalCount($price) should return the metadata count', () => {
             const globalCount = s.globalCount($price);
             globalCount._compile(fakeMetadata);
-            expect(globalCount.eval()).toEqual(4);
+            expect(globalCount.value).toEqual(4);
         });
 
         it('globalPercentile($price, 30) should return the metadata count', () => {
@@ -54,7 +54,7 @@ describe('src/core/viz/expressions/viewportAggregation', () => {
             }
             const globalPercentile = s.globalPercentile($price, 30);
             globalPercentile._compile(fakeMetadata);
-            expect(globalPercentile.eval()).toBeCloseTo(0.3 * (fakeMetadata.columns[0].max - fakeMetadata.columns[0].min) + fakeMetadata.columns[0].min, 2);
+            expect(globalPercentile.value).toBeCloseTo(0.3 * (fakeMetadata.columns[0].max - fakeMetadata.columns[0].min) + fakeMetadata.columns[0].min, 2);
         });
     });
 });
