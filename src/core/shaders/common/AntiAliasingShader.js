c28111b2ae7df3d9190cfd8ac536c0ab987d9d77
IagoLast <iago.lastra@gmail.com>
2018-05-24 16:53:12 +0200

    Rename aashader

--- src/core/shaders/common/AntiAliasingShader.js
+++ src/core/shaders/common/AntiAliasingShader.js
@@ -1,7 +1,7 @@
 import { compileProgram } from '../utils';
 import { FS, VS } from './antialiasing-glsl';
 
-export default class AABlender {
+export default class AntiAliasingShader {
     constructor(gl) {
         const { program, programID } = compileProgram(gl, VS, FS);
         this.program = program;
