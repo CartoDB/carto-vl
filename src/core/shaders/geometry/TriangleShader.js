6a93689e4ed6c84af44976c75b5b1798f017cbfa
David M <dmanzanares@cartodb.com>
2018-05-31 17:21:22 +0200

    Fix polygon rendering regression from previous merge with master

--- src/core/shaders/geometry/TriangleShader.js
+++ src/core/shaders/geometry/TriangleShader.js
@@ -7,10 +7,14 @@ export default class TriangleShader {
         this.program = program;
         this.programID = programID;
         this.vertexPositionAttribute = gl.getAttribLocation(this.program, 'vertexPosition');
+        this.normalAttr = gl.getAttribLocation(this.program, 'normal');
         this.featureIdAttr = gl.getAttribLocation(this.program, 'featureID');
         this.vertexScaleUniformLocation = gl.getUniformLocation(this.program, 'vertexScale');
         this.vertexOffsetUniformLocation = gl.getUniformLocation(this.program, 'vertexOffset');
         this.colorTexture = gl.getUniformLocation(this.program, 'colorTex');
+        this.colorStrokeTexture = gl.getUniformLocation(this.program, 'strokeColorTex');
+        this.strokeWidthTexture = gl.getUniformLocation(this.program, 'strokeWidthTex');
         this.filterTexture = gl.getUniformLocation(this.program, 'filterTex');
+        this.normalScale = gl.getUniformLocation(this.program, 'normalScale');
     }
 }
