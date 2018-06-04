9d7eeea94873f7823dd5cf1e6e49219f988c434e
elenatorro <elenatorro@gmail.com>
2018-06-04 12:26:00 +0200

    Use 'serve' to run integration tests

--- test/integration/render/render.test.js
+++ test/integration/render/render.test.js
@@ -4,14 +4,33 @@ const util = require('../../common/util');
 
 chai.use(require('chai-as-promised'));
 
+const handler = require('serve-handler');
+const http = require('http');
+
 const files = util.loadFiles(path.join(__dirname, 'scenarios'));
 const template = util.loadTemplate(path.join(__dirname, 'render.html.tpl'));
+const PORT = 5000;
 
 describe('Render tests:', () => {
+    let server;
+    
+    before(() => {
+        server = http.createServer((request, response) => {
+            return handler(request, response);
+        });
+
+        server.listen(PORT);
+    });
+
+    after(() => {
+        server.close();
+    });
+    
     files.forEach(test);
 });
 
 function test(file) {
+
     it(util.getName(file), () => {
         const actual = util.testSST(file, template);
         // Temporary threshold (1px) to cover small renderer differences between Mac & Linux
