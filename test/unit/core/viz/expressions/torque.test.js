0c8b8051e1014d0ca482cbe39a26b5a5ddcd30a9
IagoLast <iago.lastra@gmail.com>
2018-06-13 11:28:17 +0200

    Give tests some space

--- test/unit/core/viz/expressions/torque.test.js
+++ test/unit/core/viz/expressions/torque.test.js
@@ -9,19 +9,23 @@ describe('src/core/viz/expressions/torque', () => {
         validateTypeErrors('torque', ['color', 10]);
         validateTypeErrors('torque', ['number', 'color']);
     });
+    
     describe('type', () => {
         validateStaticType('torque', ['number'], 'number');
         validateStaticType('torque', ['number', 10], 'number');
     });
+    
     describe('.eval()', () => {
         it('should eval to 0 when the input is 1', () => {
             expect(s.torque(1).eval()).toEqual(0);
         });
+        
         it('should eval close to 1 when the input is 0 and the fading is high', () => {
             const t = s.torque(0, 10, s.fade(10));
             t._setTimestamp(0);
             expect(t.eval()).toEqual(1);
         });
+        
         it('should eval close to 0.75 when the input is 0 and we have wait a quarter of the animation', () => {
             const t = s.torque(0, 1, s.fade(1));
             t._setTimestamp(0);
