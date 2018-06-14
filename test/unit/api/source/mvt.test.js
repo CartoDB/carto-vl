d7a2b97250089184805cde978faa211df93ecd29
David Manzanares <dmanzanares@cartodb.com>
2018-06-14 17:44:27 +0200

    Fix unit tests

--- test/unit/api/source/mvt.test.js
+++ test/unit/api/source/mvt.test.js
@@ -2,28 +2,10 @@ import MVT from '../../../../src/api/source/mvt';
 
 describe('api/source/mvt', () => {
     describe('constructor', () => {
-        it('should build a new Source passing a MVT url', () => {
-            const metadata = { columns: [{ type: 'number', name: 'test_col' }] };
-            const source = new MVT('http://www.example.com/mvt/{z}/{x}/{y}.mvt', metadata);
-            expect(source._templateURL).toEqual('http://www.example.com/mvt/{z}/{x}/{y}.mvt');
-            expect(source.metadata).toBeDefined();
-            expect(source.metadata.columns).toEqual([{ type: 'number', name: 'test_col' }]);
-        });
-        it('should build a new Source passing a MVT url and multiple column types', () => {
-            const metadata = { columns: [{ type: 'number', name: 'test_col' }, { type: 'category', name: 'cat_col' }] };
-            const source = new MVT('http://www.example.com/mvt/{z}/{x}/{y}.mvt', metadata);
-            expect(source._templateURL).toEqual('http://www.example.com/mvt/{z}/{x}/{y}.mvt');
-            expect(source.metadata).toBeDefined();
-            expect(source.metadata.columns).toEqual(metadata.columns);
-        });
         it('should check for invalid input parameterstemplate urls', () => {
-            const metadata = { columns: [{ type: 'number', name: 'test_col' }] };
             expect(function () {
-                new MVT('http://www.example/mvt/{z}/{x}/{y}.mvt', metadata);
+                new MVT('http://www.example/mvt/{z}/{x}/{y}.mvt');
             }).toThrowError('`templateURL` property is not a valid URL.');
-            expect(function () {
-                new MVT('http://www.example.com/mvt/{z}/{x}/{y}.mvt');
-            }).toThrowError('`metadata` property is required for MVT source.');
         });
     });
 });
--- test/unit/client/windshaft.test.js
+++ test/unit/client/windshaft.test.js
@@ -1,20 +1,20 @@
 import Windshaft from '../../../src/client/windshaft';
 
 describe('src/client/windshaft', () => {
-    describe('_getCategoryIDFromString', () => {
+    describe('_categorizeString', () => {
         it('should add categories', () => {
             const w = new Windshaft(null);
-            w._getCategoryIDFromString('cat1', false);
-            w._getCategoryIDFromString('cat2', false);
-            expect(w._getCategoryIDFromString('cat1')).toEqual(0);
-            expect(w._getCategoryIDFromString('cat2')).toEqual(1);
+            w._categorizeString('cat1', false);
+            w._categorizeString('cat2', false);
+            expect(w._categorizeString('cat1')).toEqual(0);
+            expect(w._categorizeString('cat2')).toEqual(1);
         });
         it('should manage null categories', () => {
             const w = new Windshaft(null);
-            w._getCategoryIDFromString('cat1', false);
-            w._getCategoryIDFromString('cat2', false);
-            w._getCategoryIDFromString('null', false);
-            expect(w._getCategoryIDFromString(undefined)).toEqual(2);
+            w._categorizeString('cat1', false);
+            w._categorizeString('cat2', false);
+            w._categorizeString('null', false);
+            expect(w._categorizeString(undefined)).toEqual(2);
         });
     });
 });
