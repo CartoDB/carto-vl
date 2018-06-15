5feb09814655de1272f1f2d270a657de24170907
David Manzanares <dmanzanares@cartodb.com>
2018-06-15 12:00:18 +0200

    Replace some `var` with `const`

--- src/api/url.js
+++ src/api/url.js
@@ -1,14 +1,14 @@
 import CartoValidationError from './error-handling/carto-validation-error';
 
 export function validateServerURL(serverURL) {
-    var urlregex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+    const urlregex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
     if (!serverURL.match(urlregex)) {
         throw new CartoValidationError('source', 'nonValidServerURL');
     }
 }
 
 export function validateTemplateURL(templateURL) {
-    var urlregex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=\{\}]*)/;
+    const urlregex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=\{\}]*)/;
     if (!templateURL.match(urlregex)) {
         throw new CartoValidationError('source', 'nonValidTemplateURL');
     }
