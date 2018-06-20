119186b2fe1b91403f564e8be3e275b668515e41
IagoLast <iago.lastra@gmail.com>
2018-06-20 16:13:15 +0200

    Add benchmark POC

---
+++ test/benchmark/perf.test.js
@@ -0,0 +1,42 @@
+
+import Dataframe from '../../src/core/dataframe';
+
+const dataframe = new Dataframe({
+    center: { x: 0, y: 0 },
+    scale: 1,
+    geom: [
+        0, 0,
+        1, 1
+    ],
+    properties: {
+        id: [1, 2],
+        cartodb_id: [0, 1]
+    },
+    type: 'point',
+    size: 2,
+    active: true,
+    metadata: {
+        columns: [{
+            name: 'id',
+            type: 'number'
+        },
+        {
+            name: 'cartodb_id',
+            type: 'number'
+        }]
+    }
+});
+
+const viz = {
+    width: { eval: () => 0.5 },
+    strokeWidth: { eval: () => 0.5 },
+    filter: { eval: () => 1. },
+};
+dataframe.renderer = { _zoom: 1, gl: { canvas: { clientHeight: 1024 } } };
+
+
+suite('foo', () => {
+    benchmark('getFeaturesAtPosition', () => {
+        dataframe.getFeaturesAtPosition({ x: 0.5, y: 0.5 }, viz);
+    });
+});
