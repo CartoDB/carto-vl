a4d93f685a57c76a6da7d1875832f9e36433ac09
Javier Goizueta <jgoizueta@gmail.com>
2018-07-02 14:50:07 +0200

    Check type of viewportFeatures arguments and prevent compilation
    Only properties supported ATM (e.g. no variables)

--- src/core/viz/expressions/viewportFeatures.js
+++ src/core/viz/expressions/viewportFeatures.js
@@ -1,16 +1,23 @@
 import BaseExpression from './base';
 import * as schema from '../../../core/schema';
+import Property from './basic/property';
 
 export class ViewportFeatures extends BaseExpression {
     constructor(...properties) {
-        // TODO: check properties type
+        if (properties.some(p => !(p instanceof Property))) {
+            throw new Error('viewportFeatures arguments can only be properties');
+        }
 
-        super({}); // { properties: properties } ?
+        super({});
         this.expr = [];
         this.type = 'featureList';
         this._isViewport = true;
 
         this._requiredProperties = properties;
+    }
+
+    _compile(metadata) {
+        throw new Error('viewportFeatures cannot be used in visualizations');
     }
 
     _getMinimumNeededSchema() {
