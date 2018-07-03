0a811d6583d4554f94c40105f57164d5a440a629
David Manzanares <dmanzanares@cartodb.com>
2018-07-03 15:03:13 +0200

    Add regression test for linear with variables

---
+++ test/integration/render/scenarios/regression/linear-variable/scenario.js
@@ -0,0 +1,10 @@
+const map = new carto.Map({
+    container: 'map',
+    background: 'black'
+});
+
+const source = new carto.source.GeoJSON(sources['points']);
+const viz = new carto.Viz('color: ramp(linear(@num), PRISM), width: 50, @num: $numeric');
+const layer = new carto.Layer('layer', source, viz);
+
+layer.addTo(map);
