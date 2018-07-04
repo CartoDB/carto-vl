56763a7fb7e8edb862799685d7855c594f10befa
David Manzanares <dmanzanares@cartodb.com>
2018-07-04 17:38:29 +0200

    Add builtin sprite test & fix render:prepare script

---
+++ test/integration/render/scenarios/sprites/builtin/scenario.js
@@ -0,0 +1,15 @@
+const map = new carto.Map({
+    container: 'map',
+    background: 'white'
+});
+
+const source = new carto.source.GeoJSON(sources['points']);
+const viz = new carto.Viz(`
+width: 50
+color: ramp(linear($numeric), prism)
+symbol: markerOutline
+symbolPlacement: align_bottom
+`);
+const layer = new carto.Layer('layer', source, viz);
+
+layer.addTo(map);
