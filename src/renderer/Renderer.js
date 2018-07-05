a02cfed4273599459980129959ff43b2d4ddfe7e
Javier Goizueta <jgoizueta@gmail.com>
2018-07-05 22:38:21 +0200

    Fix dataframe features cache
    Features where cached with incorrect indices

--- src/renderer/Renderer.js
+++ src/renderer/Renderer.js
@@ -230,7 +230,7 @@ export default class Renderer {
      */
     _featureFromDataFrame(dataframe, index) {
         if (!dataframe.cachedFeatures) {
-            dataframe.cachedFeatures = [];
+            dataframe.cachedFeatures = new Array(dataframe.numFeatures);
         }
 
         if (dataframe.cachedFeatures[index] !== undefined) {
@@ -243,7 +243,7 @@ export default class Renderer {
             const name = propertyNames[i];
             feature[name] = dataframe.properties[name][index];
         }
-        dataframe.cachedFeatures.push(feature);
+        dataframe.cachedFeatures[index] = feature;
         return feature;
     }
 
