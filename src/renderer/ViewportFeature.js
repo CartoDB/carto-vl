79eb4942467cab3a9aa1abc721172b6fce2f771a
elenatorro <elenatorro@gmail.com>
2018-07-06 10:48:42 +0200

    Change getters

--- src/renderer/ViewportFeature.js
+++ src/renderer/ViewportFeature.js
@@ -4,24 +4,16 @@ export default class ViewportFeature {
         this._properties = properties;
         this._metadata = metadata;
 
-        this.setProperties();
+        this._setProperties();
     }
 
-    setProperties() {
+    _setProperties() {
         this._properties.forEach((name) => {
             Object.defineProperty(this.prototype, name, {
                 get: this._metadata.properties[name].type === 'category'
-                    ? _getMetadataProperty(name).call(this)
-                    : _getFeatureProperty(name).call(this)
+                    ? () => this._feature[name]
+                    : () => this.metadata.IDToCategory.get(this._feature[name])
             });
         });
     }
-}
-
-function _getFeatureProperty(name) {
-    return () => this._feature[name];
-}
-
-function _getMetadataProperty(name) {
-    return () => this.metadata.IDToCategory.get(this._feature[name]);
 }
