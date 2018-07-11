a3bccc4ada25e9600b27f98a09bfb9df661d77b9
Raul Ochoa <rochoaf@gmail.com>
2018-07-11 20:19:28 +0000

    Going red: viewportZoomToSourceZoom is undefined
    The option is not defined when other options are provided.

--- test/unit/source/mvt.test.js
+++ test/unit/source/mvt.test.js
@@ -1,5 +1,34 @@
+import Mvt from '../../../src/sources/Mvt';
 
 describe('sources/mvt', () => {
     describe('constructor', () => {
+        const metadata = {
+            idProperty: 'id',
+            properties: {}
+        };
+        const layerId = 'layer0';
+        const maxZoom = 14;
+
+        it('should have viewportZoomToSourceZoom when no options are provided', () => {
+            const source = new Mvt('URL', metadata);
+            expect(source._options.viewportZoomToSourceZoom).not.toBeUndefined();
+        });
+
+        it('should have viewportZoomToSourceZoom when layerId option is present but viewportZoomToSourceZoom is not', () => {
+            const source = new Mvt('URL', metadata, { layerId });
+            expect(source._options.viewportZoomToSourceZoom).not.toBeUndefined();
+        });
+
+        it('should have viewportZoomToSourceZoom when layerId and maxZoom option is present but viewportZoomToSourceZoom is not', () => {
+            const source = new Mvt('URL', metadata, { layerId, maxZoom });
+            expect(source._options.viewportZoomToSourceZoom).not.toBeUndefined();
+        });
+
+        it('should have viewportZoomToSourceZoom as provided', () => {
+            const viewportZoomToSourceZoom = z => z;
+            const source = new Mvt('URL', metadata, { viewportZoomToSourceZoom });
+            expect(source._options.viewportZoomToSourceZoom).not.toBeUndefined();
+            expect(source._options.viewportZoomToSourceZoom).toEqual(viewportZoomToSourceZoom);
+        });
     });
 });
