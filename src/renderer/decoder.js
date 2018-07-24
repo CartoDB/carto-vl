279327487b583bfe3aa3409e5aec8e243a7b2e4a
elenatorro <elenatorro@gmail.com>
2018-07-24 10:04:39 +0200

    Improve decoder performance

--- src/renderer/decoder.js
+++ src/renderer/decoder.js
@@ -19,16 +19,16 @@ import { getJointNormal, getLineNormal } from '../utils/geometry';
 // If the geometry type is 'line' it will generate the appropriate zero-sized, vertex-shader expanded triangle list with mitter joints.
 // The geom will be an array of coordinates in this case`
 export function decodeGeom (geomType, geom) {
-    if (geomType === 'point') {
-        return decodePoint(geom);
+    switch (geomType) {
+        case 'point':
+            return decodePoint(geom);
+        case 'polygon':
+            return decodePolygon(geom);
+        case 'line':
+            return decodeLine(geom);
+        default:
+            throw new Error(`Unimplemented geometry type: '${geomType}'`);
     }
-    if (geomType === 'polygon') {
-        return decodePolygon(geom);
-    }
-    if (geomType === 'line') {
-        return decodeLine(geom);
-    }
-    throw new Error(`Unimplemented geometry type: '${geomType}'`);
 }
 
 function decodePoint (vertices) {
@@ -53,27 +53,36 @@ function decodePolygon (geometry) {
     let normals = [];
     let breakpoints = []; // Array of indices (to vertexArray) that separate each feature
 
-    geometry.forEach(feature => {
-        feature.forEach(polygon => {
-            const triangles = earcut(polygon.flat, polygon.holes);
+    const geometryLength = geometry.length;
 
-            for (let i = 0; i < triangles.length; i++) {
-                const index = triangles[i];
+    for (let i = 0; i < geometryLength; i++) {
+        const feature = geometry[i];
+        const featureLength = feature.length;
+
+        for (let j = 0; j < featureLength; j++) {
+            const polygon = feature[j];
+            const triangles = earcut(polygon.flat, polygon.holes);
+            const trianglesLength = triangles.length;
+
+            for (let k = 0; k < trianglesLength; k++) {
+                const index = triangles[k];
                 vertices.push(polygon.flat[2 * index], polygon.flat[2 * index + 1]);
                 normals.push(0, 0);
             }
 
             const lineString = polygon.flat;
-            for (let i = 0; i < lineString.length - 2; i += 2) {
-                if (polygon.holes.includes((i + 2) / 2)) {
+            const lineStringLength = lineString.length;
+
+            for (let l = 0; l < lineStringLength - 2; l += 2) {
+                if (polygon.holes.includes((l + 2) / 2)) {
                     // Skip adding the line which connects two rings
                     continue;
                 }
 
-                const a = [lineString[i + 0], lineString[i + 1]];
-                const b = [lineString[i + 2], lineString[i + 3]];
+                const a = [lineString[l + 0], lineString[l + 1]];
+                const b = [lineString[l + 2], lineString[l + 3]];
 
-                if (isClipped(polygon, i, i + 2)) {
+                if (isClipped(polygon, l, l + 2)) {
                     continue;
                 }
 
@@ -107,10 +116,10 @@ function decodePolygon (geometry) {
                 vertices.push(b[0], b[1]);
                 vertices.push(b[0], b[1]);
             }
-        });
+        }
 
         breakpoints.push(vertices.length);
-    });
+    }
 
     return {
         vertices: new Float32Array(vertices),
@@ -119,28 +128,34 @@ function decodePolygon (geometry) {
     };
 }
 
-function decodeLine (geom) {
+function decodeLine (geometry) {
     let vertices = [];
     let normals = [];
     let breakpoints = []; // Array of indices (to vertexArray) that separate each feature
 
-    geom.map(feature => {
-        feature.map(lineString => {
-            // Create triangulation
+    const geometryLength = geometry.length;
 
-            for (let i = 0; i < lineString.length - 2; i += 2) {
-                const a = [lineString[i + 0], lineString[i + 1]];
-                const b = [lineString[i + 2], lineString[i + 3]];
+    for (let i = 0; i < geometryLength; i++) {
+        const feature = geometry[i];
+        const featureLength = feature.length;
+
+        for (let j = 0; j < featureLength; j++) {
+            const lineString = feature[j];
+            const lineStringLength = lineString.length;
+
+            for (let k = 0; k < lineStringLength - 2; k += 2) {
+                const a = [lineString[k + 0], lineString[k + 1]];
+                const b = [lineString[k + 2], lineString[k + 3]];
                 const normal = getLineNormal(b, a);
                 let na = normal;
                 let nb = normal;
 
-                if (i > 0) {
-                    const prev = [lineString[i - 2], lineString[i - 1]];
+                if (k > 0) {
+                    const prev = [lineString[k - 2], lineString[k - 1]];
                     na = getJointNormal(prev, a, b) || na;
                 }
-                if (i < lineString.length - 4) {
-                    const next = [lineString[i + 4], lineString[i + 5]];
+                if (k < lineString.length - 4) {
+                    const next = [lineString[k + 4], lineString[k + 5]];
                     nb = getJointNormal(a, b, next) || nb;
                 }
 
@@ -164,10 +179,10 @@ function decodeLine (geom) {
                 vertices.push(b[0], b[1]);
                 vertices.push(b[0], b[1]);
             }
-        });
+        }
 
         breakpoints.push(vertices.length);
-    });
+    }
 
     return {
         vertices: new Float32Array(vertices),
