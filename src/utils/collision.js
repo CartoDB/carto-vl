e7902caa3ed81081e70355b813a20766250c3d83
elenatorro <elenatorro@gmail.com>
2018-07-26 09:50:54 +0200

    Refactor

--- src/utils/collision.js
+++ src/utils/collision.js
@@ -1,7 +1,7 @@
 import { halfPlaneSign } from './geometry';
 
-const separatingLineFound = 'separatingLineFound';
-const noSeparatingLineFound = 'noSeparatingLineFound';
+const SEPARATING_LINE_FOUND = 'separatingLineFound';
+const SEPARATING_LINE_NOT_FOUND = 'separatingLineNotFound';
 
 export function triangleCollides (triangle, viewportAABB) {
     const viewport = [
@@ -11,89 +11,99 @@ export function triangleCollides (triangle, viewportAABB) {
         { x: viewportAABB.maxx, y: viewportAABB.maxy }
     ];
 
-    // possible optimizations (requires profiling)
-    // if (_isAnyTriangleVertexInViewport(triangle, viewportAABB)) {
-    //     return true;
-    // }
+    /*
+     * TODO
+     *
+     * Optimize triangle collision:
+     *
+     * if (_isAnyTriangleVertexInViewport(triangle, viewportAABB)) {
+     *   return true;
+     * }
+     *
+     * if (_isAnyViewportVertexInTriangle(triangle, viewport)) {
+     *   return true;
+     * }
+     */
 
-    // if (_isAnyViewportVertexInTriangle(triangle, viewport)) {
-    //     return true;
-    // }
-    if (_viewportLineSeparatesTriangle(viewportAABB, triangle) === separatingLineFound) {
+    if (_viewportLineSeparatesTriangle(viewportAABB, triangle) === SEPARATING_LINE_FOUND) {
         return false;
     }
 
-    if (_triangleLineSeparatesViewport(triangle, viewport) === separatingLineFound) {
+    if (_triangleLineSeparatesViewport(triangle, viewport) === SEPARATING_LINE_FOUND) {
         return false;
     }
 
     return true;
 }
 
-// These functions could be used to optimize `triangleCollides`, but it requires profiling
-// function _isAnyViewportVertexInTriangle (triangle, viewport) {
-//     for (let i = 0; i < viewport.length; i++) {
-//         if (pointInTriangle(viewport[i], triangle[0], triangle[1], triangle[2])) {
-//             return true;
-//         }
-//     }
-
-//     return false;
-// }
-
-// function _isAnyTriangleVertexInViewport (triangle, viewportAABB) {
-//     for (let i = 0; i < 3; i++) {
-//         if (pointInRectangle(triangle[i], viewportAABB)) {
-//             return true;
-//         }
-//     }
-
-//     return false;
-// }
+/*
+ * TODO
+ *
+ * Optimize triangle collision:
+ *
+ * function _isAnyViewportVertexInTriangle (triangle, viewport) {
+ *     for (let i = 0; i < viewport.length; i++) {
+ *         if (pointInTriangle(viewport[i], triangle[0], triangle[1], triangle[2])) {
+ *             return true;
+ *         }
+ *     }
+ *
+ *     return false;
+ * }
+ * function _isAnyTriangleVertexInViewport (triangle, viewportAABB) {
+ *  for (let i = 0; i < 3; i++) {
+ *       if (pointInRectangle(triangle[i], viewportAABB)) {
+ *           return true;
+ *       }
+ *   }
+ *   return false;
+ * }
+ */
 
 function _viewportLineSeparatesTriangle (viewportAABB, triangle) {
     if (triangle[0].x < viewportAABB.minx &&
         triangle[1].x < viewportAABB.minx &&
         triangle[2].x < viewportAABB.minx) {
-        // is separating line
-        return separatingLineFound;
+        return SEPARATING_LINE_FOUND;
     }
+
     if (triangle[0].y < viewportAABB.miny &&
         triangle[1].y < viewportAABB.miny &&
         triangle[2].y < viewportAABB.miny) {
-        // is separating line
-        return separatingLineFound;
+        return SEPARATING_LINE_FOUND;
     }
+
     if (triangle[0].x > viewportAABB.maxx &&
         triangle[1].x > viewportAABB.maxx &&
         triangle[2].x > viewportAABB.maxx) {
-        // is separating line
-        return separatingLineFound;
+        return SEPARATING_LINE_FOUND;
     }
+
     if (triangle[0].y > viewportAABB.maxy &&
         triangle[1].y > viewportAABB.maxy &&
         triangle[2].y > viewportAABB.maxy) {
-        // is separating line
-        return separatingLineFound;
+        return SEPARATING_LINE_FOUND;
     }
-    return noSeparatingLineFound;
+
+    return SEPARATING_LINE_NOT_FOUND;
 }
 
 function _triangleLineSeparatesViewport (triangle, viewport) {
-    for (let i = 0; i < 3; i++) {
+    const TRIANGLE_VERTICES = 3;
+
+    for (let i = 0; i < TRIANGLE_VERTICES; i++) {
         for (let j = 0; j < viewport.length; j++) {
-            const position = halfPlaneSign(viewport[j], triangle[i], triangle[(i + 1) % 3]);
+            const position = halfPlaneSign(viewport[j], triangle[i], triangle[(i + 1) % TRIANGLE_VERTICES]);
 
             if (position > 0) {
                 break;
             }
 
-            if (j === 3) {
-                // is separating line
-                return separatingLineFound;
+            if (j === TRIANGLE_VERTICES) {
+                return SEPARATING_LINE_FOUND;
             }
         }
     }
 
-    return noSeparatingLineFound;
+    return SEPARATING_LINE_NOT_FOUND;
 }
