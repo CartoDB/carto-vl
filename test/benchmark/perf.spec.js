693681499b7877713b3f7b2eb93365dfdf90625c
David Manzanares <dmanzanares@cartodb.com>
2018-07-27 14:37:28 +0200

    Fix linting

--- test/benchmark/perf.spec.js
+++ test/benchmark/perf.spec.js
@@ -2,23 +2,23 @@ import { decodeGeom } from '../../src/renderer/decoder';
 import GeoJSON from '../../src/sources/GeoJSON';
 
 const geojson = new GeoJSON({
-    "type": "Feature",
-    "properties": {
-      "name": "Bermuda Triangle",
-      "area": 1150180
+    'type': 'Feature',
+    'properties': {
+        'name': 'Bermuda Triangle',
+        'area': 1150180
     },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        [
-          [-64.73, 32.31],
-          [-80.19, 25.76],
-          [-66.09, 18.43],
-          [-64.73, 32.31]
+    'geometry': {
+        'type': 'Polygon',
+        'coordinates': [
+            [
+                [-64.73, 32.31],
+                [-80.19, 25.76],
+                [-66.09, 18.43],
+                [-64.73, 32.31]
+            ]
         ]
-      ]
     }
-  });
+});
 const polygonGeometry = geojson._decodeGeometry();
 
 falcon.benchmark('decodePolygon', () => {
