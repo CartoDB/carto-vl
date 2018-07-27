b9ed15feed208f570d1fa5042774cf737e5c2c41
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-07-27 14:44:35 +0200

    Line decoder loop optimization

--- src/renderer/decoder/lineDecoder.js
+++ src/renderer/decoder/lineDecoder.js
@@ -8,17 +8,22 @@ export function decodeLine (geometry) {
     let vertices = [];
     let normals = [];
     let breakpoints = []; // Array of indices (to vertexArray) that separate each feature
-
     let featureIDToVertexIndex = new Map();
-    geometry.map(feature => {
-        feature.map(lineString => {
+
+    for (let i = 0; i < geometry.length; i++) {
+        const feature = geometry[i];
+        for (let j = 0; j < feature.length; j++) {
+            const lineString = feature[j];
             addLine(lineString, vertices, normals);
-        });
+        }
+
         featureIDToVertexIndex.set(breakpoints.length, breakpoints.length === 0
             ? { start: 0, end: vertices.length }
             : { start: featureIDToVertexIndex.get(breakpoints.length - 1).end, end: vertices.length });
+
         breakpoints.push(vertices.length);
-    });
+    }
+
     return {
         vertices: getFloat32ArrayFromArray(vertices),
         normals: getFloat32ArrayFromArray(normals),
