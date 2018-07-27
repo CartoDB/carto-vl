66820c360c2745e10eaffbf203e200fe7462315d
elenatorro <elenatorro@gmail.com>
2018-07-27 14:49:56 +0200

    Improve rendering specs

--- test/integration/render/scenarios/classification/viewportEqIntervalsExpression/scenario.js
+++ test/integration/render/scenarios/classification/viewportEqIntervalsExpression/scenario.js
@@ -3,7 +3,7 @@ const map = new carto.Map({
     background: 'black'
 });
 
-const source = new carto.source.GeoJSON(sources['points']);
+const source = new carto.source.GeoJSON(sources['many-points']);
 const viz = new carto.Viz('color: ramp(viewportEqIntervals(add($numeric, 100), 5), PRISM), width: 50');
 const layer = new carto.Layer('layer', source, viz);
 
--- test/integration/render/scenarios/classification/viewportQuantilesExpression/scenario.js
+++ test/integration/render/scenarios/classification/viewportQuantilesExpression/scenario.js
@@ -3,7 +3,7 @@ const map = new carto.Map({
     background: 'black'
 });
 
-const source = new carto.source.GeoJSON(sources['points']);
+const source = new carto.source.GeoJSON(sources['many-points']);
 const viz = new carto.Viz('color: ramp(viewportQuantiles(mul($numeric, 5), 3), PRISM), width: 50');
 const layer = new carto.Layer('layer', source, viz);
 
