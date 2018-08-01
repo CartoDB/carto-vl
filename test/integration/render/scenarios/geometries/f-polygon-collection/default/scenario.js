fc7cb8ac36611a5fc64405fdfeae5a5cf76b04ce
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-08-01 12:30:42 +0200

    Add polygon-collection render tests

---
+++ test/integration/render/scenarios/geometries/f-polygon-collection/default/scenario.js
@@ -0,0 +1,15 @@
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: { version: 8, sources: {}, layers: [] },
+    center: [0, 40],
+    zoom: 2
+});
+
+const source = new carto.source.GeoJSON(sources['polygon-collection']);
+const viz = new carto.Viz();
+const layer = new carto.Layer('layer', source, viz);
+
+layer.addTo(map);
+layer.on('loaded', () => {
+    window.loaded = true;
+});
