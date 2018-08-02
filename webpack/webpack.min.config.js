a98fdf98bc67cef6ad1ad372c11c0c660be86f9b
David M <dmanzanares@cartodb.com>
2018-08-02 18:22:50 +0200

    Remove breaking Uglify plugin from minifiez build

--- webpack/webpack.min.config.js
+++ webpack/webpack.min.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const banner = require('./banner');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -17,37 +16,6 @@ module.exports = {
         rules: [
             { test: /\.glsl$/, use: 'webpack-glsl-loader' },
             { test: /\.svg$/, use: 'svg-inline-loader' }
-        ]
-    },
-    optimization: {
-        minimizer: [
-            new UglifyJSPlugin({
-                uglifyOptions: {
-                    output: {
-                        comments: false
-                    },
-                    compress: {
-                        properties: true,
-                        keep_fargs: false,
-                        pure_getters: true,
-                        collapse_vars: true,
-                        warnings: false,
-                        sequences: true,
-                        dead_code: true,
-                        drop_debugger: true,
-                        comparisons: true,
-                        conditionals: true,
-                        evaluate: true,
-                        booleans: true,
-                        loops: true,
-                        unused: true,
-                        hoist_funs: true,
-                        if_return: true,
-                        join_vars: true,
-                        drop_console: true
-                    }
-                }
-            })
         ]
     },
     plugins: [
