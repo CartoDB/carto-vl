9574d268278c10a1c165b48096b26df4a5c8c171
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-08-02 16:58:01 +0200

    Optimize line decoder

--- src/renderer/decoder/lineDecoder.js
+++ src/renderer/decoder/lineDecoder.js
@@ -4,28 +4,30 @@ import { getFloat32ArrayFromArray } from '../../utils/util';
 // If the geometry type is 'line' it will generate the appropriate zero-sized, vertex-shader expanded triangle list with `miter` and `bevel` joins.
 // The geom will be an array of coordinates in this case
 
+let vertices = new Array(1000000);
+let normals = new Array(1000000);
+
 export function decodeLine (geometry) {
-    let vertices = [];
-    let normals = [];
     let breakpoints = []; // Array of indices (to vertexArray) that separate each feature
     let featureIDToVertexIndex = new Map();
+    let vertexIndex = 0;
 
     for (let i = 0; i < geometry.length; i++) {
         const feature = geometry[i];
         for (let j = 0; j < feature.length; j++) {
-            addLine(feature[j], vertices, normals);
+            vertexIndex = addLine(feature[j], vertices, normals, vertexIndex);
         }
 
         featureIDToVertexIndex.set(breakpoints.length, breakpoints.length === 0
-            ? { start: 0, end: vertices.length }
-            : { start: featureIDToVertexIndex.get(breakpoints.length - 1).end, end: vertices.length });
+            ? { start: 0, end: vertexIndex }
+            : { start: featureIDToVertexIndex.get(breakpoints.length - 1).end, end: vertexIndex });
 
-        breakpoints.push(vertices.length);
+        breakpoints.push(vertexIndex);
     }
 
     return {
-        vertices: getFloat32ArrayFromArray(vertices),
-        normals: getFloat32ArrayFromArray(normals),
+        vertices: getFloat32ArrayFromArray(vertices, vertexIndex),
+        normals: getFloat32ArrayFromArray(normals, vertexIndex),
         featureIDToVertexIndex,
         breakpoints
     };
