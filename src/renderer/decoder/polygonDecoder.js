9ee9c27dc5ae8e7b4720b9b3643aeafa7b3c5a60
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-08-04 23:59:24 +0200

    Up to 4MB

--- src/renderer/decoder/polygonDecoder.js
+++ src/renderer/decoder/polygonDecoder.js
@@ -13,7 +13,7 @@ import { addLineString } from './common';
      }]
 */
 
-const STATIC_INITIAL_BUFFER_SIZE = 1024 * 1024 / 4; // 1 MB
+const STATIC_INITIAL_BUFFER_SIZE = 1024 * 1024; // 4 MB
 const geomBuffer = {
     index: 0,
     vertices: new Float32Array(STATIC_INITIAL_BUFFER_SIZE),
