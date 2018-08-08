5584da120dedcc482c6fca8c3cbd6e0dfd7be7e9
David Manzanares <dmanzanares@cartodb.com>
2018-08-08 12:26:06 +0200

    Documente MVT templateURLs correctly

--- src/sources/MVT.js
+++ src/sources/MVT.js
@@ -86,13 +86,20 @@ export default class MVT extends Base {
     /**
      * Create a carto.source.MVT.
      *
-     * @param {object} data - A MVT data object
+     * @param {string | string[]} templateURL - A string with the URL template of the MVT tiles in https://mytileserver.com/{z}/{x}/{y}.mvt format or a list of such templates. Usage of a list of templates with different domains is recommended since that allows the browser to make more requests in parallel.
      * @param {MVTMetadata} [metadata] - Metadata of the source, declaring property name, types and optionally ranges.
      * @param {MVTOptions} [options] - MVT source configuration, the default value will be valid for regular URL templates if the tiles are composed of only one layer
      *
-     * @example
+     * @example Usage with multiple templateURLs as recommended
      * const metadata = new carto.source.mvt.Metadata([{ type: 'number', name: 'total_pop'}])
-     * new carto.source.MVT("https://{server}/{z}/{x}/{y}.mvt", metadata);
+     * new carto.source.MVT([
+     *                       "https://server-a.tileserver.com/{z}/{x}/{y}.mvt",
+     *                       "https://server-b.tileserver.com/{z}/{x}/{y}.mvt",
+     *                       "https://server-c.tileserver.com/{z}/{x}/{y}.mvt",
+     *                       "https://server-d.tileserver.com/{z}/{x}/{y}.mvt"
+     *                      ],
+     *                      metadata
+     *                     );
      *
      * @fires CartoError
      *
