26388da27f07ebf8dabeca34dfbe3509a66ea19c
David M <dmanzanares@cartodb.com>
2018-08-17 14:57:35 +0200

    Add unit tests to zoomrange

---
+++ test/unit/renderer/viz/expressions/Zoomrange.test.js
@@ -0,0 +1,12 @@
+import { validateStaticType, validateCompileTypeError } from './utils';
+
+describe('src/renderer/viz/expressions/zoom', () => {
+    describe('type', () => {
+        validateStaticType('zoomrange', [[7, 10]], 'number');
+    });
+
+    describe('error control', () => {
+        validateCompileTypeError('zoomrange', []);
+        validateCompileTypeError('zoomrange', [0]);
+    });
+});
