00c77efad02fde359909e97a10d7823efcc1db78
David M <dmanzanares@cartodb.com>
2018-08-17 15:12:19 +0200

    Fix wrong test names

--- test/unit/renderer/viz/expressions/Scale.test.js
+++ test/unit/renderer/viz/expressions/Scale.test.js
@@ -1,6 +1,6 @@
 import { validateStaticType } from './utils';
 
-describe('src/renderer/viz/expressions/zoom', () => {
+describe('src/renderer/viz/expressions/scale', () => {
     describe('type', () => {
         validateStaticType('scale', [1], 'number');
     });
--- test/unit/renderer/viz/expressions/Zoomrange.test.js
+++ test/unit/renderer/viz/expressions/Zoomrange.test.js
@@ -1,6 +1,6 @@
 import { validateStaticType, validateCompileTypeError } from './utils';
 
-describe('src/renderer/viz/expressions/zoom', () => {
+describe('src/renderer/viz/expressions/zoomrange', () => {
     describe('type', () => {
         validateStaticType('zoomrange', [[7, 10]], 'number');
     });
