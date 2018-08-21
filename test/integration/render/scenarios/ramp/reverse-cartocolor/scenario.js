bf4182e397b6cf0e5a77b08b4bfc1d1ccf3269a0
David M <dmanzanares@cartodb.com>
2018-08-21 13:39:06 +0200

    Add render tests to reverse()

---
+++ test/integration/render/scenarios/ramp/reverse-cartocolor/scenario.js
@@ -0,0 +1,13 @@
+const map = new carto.Map({
+    container: 'map',
+    background: 'black'
+});
+
+const source = new carto.source.GeoJSON(sources['points']);
+const viz = new carto.Viz('color: ramp(linear($numeric, 0, 10), reverse(prism)), width: 50');
+const layer = new carto.Layer('layer', source, viz);
+
+layer.addTo(map);
+layer.on('loaded', () => {
+    window.loaded = true;
+});
