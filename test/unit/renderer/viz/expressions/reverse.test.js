d05f38d082fc0ea1ec95aebe47ba218b83396e5d
David M <dmanzanares@cartodb.com>
2018-08-21 13:34:14 +0200

    Add some unit tests to reverse()

---
+++ test/unit/renderer/viz/expressions/reverse.test.js
@@ -0,0 +1,15 @@
+import { validateStaticType, validateMaxArgumentsError } from './utils';
+
+describe('src/renderer/viz/expressions/reverse', () => {
+    describe('type', () => {
+        validateStaticType('reverse', [[1, 2]], 'number-array');
+        validateStaticType('reverse', [['A', 'B']], 'category-array');
+        validateStaticType('reverse', ['color-array'], 'color-array');
+        validateStaticType('reverse', ['palette'], 'palette');
+        validateMaxArgumentsError('reverse', [[1, 2], 0]);
+        validateMaxArgumentsError('reverse', ['palette', 0]);
+    });
+    describe('eval', () => {
+        // TODO check reversal of the array (see blend.test.js)
+    });
+});
