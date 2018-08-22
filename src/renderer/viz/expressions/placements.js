4fe1b3e5c2e2c096740b14a7ab7ee540d4ecfbdb
elenatorro <elenatorro@gmail.com>
2018-08-22 16:32:27 +0200

    Fix render tests

--- src/renderer/viz/expressions/placements.js
+++ src/renderer/viz/expressions/placements.js
@@ -2,4 +2,4 @@ import Placement from './placement';
 import Constant from './basic/constant';
 
 export const ALIGN_CENTER = new Placement(new Constant(0), new Constant(0));
-export const ALIGN_BOTTOM = new Placement(new Constant(0), new Constant(0));
+export const ALIGN_BOTTOM = new Placement(new Constant(0), new Constant(1));
--- test/integration/render/scenarios/images/builtin/scenario.js
+++ test/integration/render/scenarios/images/builtin/scenario.js
@@ -5,11 +5,12 @@ const map = new carto.Map({
 
 const source = new carto.source.GeoJSON(sources['points']);
 const viz = new carto.Viz(`
-width: 50
-color: ramp(linear($numeric), prism)
-symbol: markerOutline
-symbolPlacement: align_bottom
+    width: 50
+    color: ramp(linear($numeric), prism)
+    symbol: marker_outline
+    symbolPlacement: align_bottom
 `);
+
 const layer = new carto.Layer('layer', source, viz);
 
 layer.addTo(map);
