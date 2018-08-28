8e01e3d3e256e43cf4dc1c6488898ecb9b74d8a3
Víctor Velarde <victor@carto.com>
2018-08-28 12:36:44 +0200

    Add flag to consider no backend aggregation in clusterCount()

--- src/renderer/viz/expressions/aggregation/cluster/ClusterCount.js
+++ src/renderer/viz/expressions/aggregation/cluster/ClusterCount.js
@@ -6,20 +6,24 @@ export default class ClusterCount extends BaseExpression {
     constructor () {
         checkMaxArguments(arguments, 0, 'clusterCount');
         super({});
-        this._expressionName = 'clusterCount';
         this.type = 'number';
+        this._expressionName = 'clusterCount';
+        this._hasClusterFeatureCount = false;
     }
 
     eval (feature) {
         return Number(feature[CLUSTER_FEATURE_COUNT]) || 1;
     }
 
-    _resolveAliases () { }
+    _bindMetadata (metadata) {
+        super._bindMetadata(metadata);
+        this._hasClusterFeatureCount = metadata.properties[CLUSTER_FEATURE_COUNT] !== undefined;
+    }
 
     _applyToShaderSource (getGLSLforProperty) {
         return {
             preface: '',
-            inline: `${getGLSLforProperty(CLUSTER_FEATURE_COUNT)}`
+            inline: `${this._hasClusterFeatureCount ? getGLSLforProperty(CLUSTER_FEATURE_COUNT) : '1.'}`
         };
     }
 }
