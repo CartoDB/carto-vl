329e2ab7fd438936694ea2fce312e3d23c49f27e
Víctor Velarde <victor@carto.com>
2018-08-31 13:34:01 +0200

    Refactor and documentation for stats

--- src/renderer/viz/expressions/stats.js
+++ src/renderer/viz/expressions/stats.js
@@ -1,9 +1,29 @@
 
+/**
+ * Calculate Average
+ *
+ * @param {Number[]} values
+ * @returns {Number} - average
+ */
 export const average = (values) => values.reduce((sum, value) => sum + value, 0) / values.length;
 
+/**
+ * Calculate Variance
+ *
+ * @param {Number[]} values
+ * @param {Number} average
+ * @returns {Number[]} - calculate variance for each element in values
+ */
+export const variance = (values, average) => values.map(value => Math.pow(value - average, 2));
+
+/**
+ * Calculate Standard Deviation (STD), using population deviation formula
+ *
+ * @param {Number[]} values
+ * @returns {Number} - standard deviation
+ */
 export const standardDeviation = (values) => {
     const avg = average(values);
-    const squareDifferences = values.map(value => Math.pow(value - avg, 2));
-    const avgSquareDiff = average(squareDifferences);
-    return Math.sqrt(avgSquareDiff);
+    const avgVariance = average(variance(values, avg));
+    return Math.sqrt(avgVariance);
 };
