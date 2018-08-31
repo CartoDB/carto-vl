48f894c80ae3e0fc487a6050dc206dd332afcf6f
David M <dmanzanares@cartodb.com>
2018-08-31 11:01:14 +0200

    Minor doc addition

--- src/sources/MVTWorkers.worker.js
+++ src/sources/MVTWorkers.worker.js
@@ -1,6 +1,10 @@
 import { MVTWorker } from './MVTWorker';
 import { WindshaftWorker } from '../client/WindshaftWorker';
 
+// This file publishes a Web Worker onmessage function that will redirect incoming messages
+// based on an ID (`workerName`) defined by the message sender
+// to each different Worker implementation
+
 const workers = {
     MVT: new MVTWorker(),
     windshaft: new WindshaftWorker()
