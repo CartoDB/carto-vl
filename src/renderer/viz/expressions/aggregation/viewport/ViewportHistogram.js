d4db075bf125ed0d8715daebe241f876f35dbee4
elenatorro <elenatorro@gmail.com>
2018-09-14 16:03:28 +0200

    Fix lint

--- src/renderer/viz/expressions/aggregation/viewport/ViewportHistogram.js
+++ src/renderer/viz/expressions/aggregation/viewport/ViewportHistogram.js
@@ -133,7 +133,7 @@ function _getCategoryValue (histogram) {
     return [...histogram]
         .map(([x, y]) => {
             return { x, y };
-        })
+        });
 }
 
 function _sortNumerically (a, b) {
