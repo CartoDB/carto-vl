e26f7ef9381f096c018cf4747902e37498de3f5b
elenatorro <elenatorro@gmail.com>
2018-09-17 10:27:53 +0200

    Test ramp top with color palette without variable

---
+++ test/integration/render/scenarios/regression/cartocolor-top-copy/scenario.js
@@ -0,0 +1,14 @@
+const map = new carto.Map({
+    container: 'map',
+    background: 'black'
+});
+
+const source = new carto.source.GeoJSON(sources['points']);
+const viz = new carto.Viz('color: ramp(top($cat, 2), PRISM), width: 50, @num: 2');
+const layer = new carto.Layer('layer', source, viz);
+
+viz.variables.num.blendTo(1, 0);
+layer.addTo(map);
+layer.on('loaded', () => {
+    window.loaded = true;
+});
