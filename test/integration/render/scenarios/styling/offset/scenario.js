48eaef7f39d67552421eca5ae242323c1b95dd99
David Manzanares <dmanzanares@cartodb.com>
2018-09-25 12:02:31 +0200

    Fix render tests

--- test/integration/render/scenarios/styling/offset/scenario.js
+++ test/integration/render/scenarios/styling/offset/scenario.js
@@ -3,11 +3,11 @@ const map = new carto.Map({
     background: 'black'
 });
 
-const source = new carto.source.GeoJSON(sources['points']);
+const source = new carto.source.GeoJSON(sources['points-concentric']);
 const viz = new carto.Viz(`
     width: 20,
     color: hsv(0, linear($numeric, 0, 10), 1)
-    transform: translate(10, 10*$numeric)
+    transform: translate(4*$numeric, 4*$numeric)
 `);
 const layer = new carto.Layer('layer', source, viz);
 
