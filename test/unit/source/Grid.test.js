195d042d39c839a1f4caff6e3b13d00ff2e1958c
Víctor Velarde <victor@carto.com>
2018-09-26 18:44:38 +0200

    Add raster bands as properties in Dataframe, generated by the Grid

--- test/unit/source/Grid.test.js
+++ test/unit/source/Grid.test.js
@@ -1,9 +1,9 @@
 import Grid from '../../../src/sources/Grid';
 
-describe('sources/Grid', () => {
+fdescribe('sources/Grid', () => {
     const urlGeotiff = 'http://127.0.0.1:8080/examples/raster/small_3857.tiff';
 
-    fdescribe('constructor', () => {
+    describe('constructor', () => {
         it('should build a new Source with an URL', async () => {
             const source = new Grid(urlGeotiff);
             await source.initializationPromise;
@@ -15,18 +15,26 @@ describe('sources/Grid', () => {
             const source = new Grid(urlGeotiff);
             await source.initializationPromise;
 
+            expect(source._center).toBeDefined();
             expect(source._center.x).toBe(-419334.36645);
             expect(source._center.y).toBe(5382692.45555);
 
+            expect(source._dataframeCenter).toBeDefined();
             expect(source._dataframeCenter.x).toBe(-0.020927470585478402);
             expect(source._dataframeCenter.y).toBe(0.26863082791863346);
         });
+    });
 
-    // it('should compute the center of the coordinates', () => {
-    //     const source = new GeoJSON(urlGeotiff);
-    //     expect(source._dataframeCenter).toBeDefined();
-    //     expect(source._dataframeCenter.x).toBeCloseTo(0.0555);
-    //     expect(source._dataframeCenter.x).toBeCloseTo(0.0567);
-    // });
+    describe('dataframe', () => {
+        it('should build a DataFrame with one property per band', async () => {
+            const source = new Grid(urlGeotiff);
+            await source.initializationPromise;
+
+            const dataframe = source._buildDataFrame();
+
+            expect(dataframe).toBeTruthy();
+            expect(dataframe.properties['band0']).toBeTruthy();
+            expect(dataframe.properties['band0'][0]).toBe(0.011275325901806355); // first pixel
+        });
     });
 });
