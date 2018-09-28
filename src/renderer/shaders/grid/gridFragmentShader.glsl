b68d46d041ee04f365e4e0499f805bd6fdbcc5a1
Javier Goizueta <jgoizueta@gmail.com>
2018-09-28 11:10:58 +0200

    Remove old commented code

--- src/renderer/shaders/grid/gridFragmentShader.glsl
+++ src/renderer/shaders/grid/gridFragmentShader.glsl
@@ -9,12 +9,5 @@ $propertyPreface
 $color_preface
 
 void main(void) {
-    // FIXME: hardcoded values!
-    float scale = 1.0/0.22;
-    float offset = 0.0;
-
-    // use single band texture as gray level
-    // float value = scale*(texture2D(propertyTex0, uv)[0] - offset);
-    // gl_FragColor = vec4(value, value, value, 1.0);
     gl_FragColor = $color_inline;
 }
