b0ac8cbcab68c015c9ac3bf340f0f9822c0dba4a
David Manzanares <dmanzanares@cartodb.com>
2018-10-02 16:40:01 +0200

    Add regression test to scaled

--- test/unit/renderer/viz/expressions/Scaled.test.js
+++ test/unit/renderer/viz/expressions/Scaled.test.js
@@ -2,7 +2,8 @@ import { validateStaticType } from './utils';
 
 describe('src/renderer/viz/expressions/Scaled', () => {
     describe('type', () => {
-        validateStaticType('scaled', [1], 'number');
+        validateStaticType('scaled', ['number'], 'number');
+        validateStaticType('scaled', ['number', 'number'], 'number');
     });
 
     describe('error control', () => {
