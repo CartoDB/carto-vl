64abf6fdc046527bcfb717f283775b1b07748be4
Raul Ochoa <rochoaf@gmail.com>
2018-10-19 17:48:05 +0000

    Revert "Test it by introducing the same regression we fixed at https://github.com/CartoDB/carto-vl/pull/1042/files"
    This reverts commit 099b29587c90e42970ccdd05606e004f1bf362ce.

--- src/renderer/Renderer.js
+++ src/renderer/Renderer.js
@@ -364,7 +364,7 @@ export default class Renderer {
             gl.bindTexture(gl.TEXTURE_2D, dataframe.texFilter);
             gl.uniform1i(renderer.filterTexture, freeTexUnit);
             freeTexUnit++;
-            gl.uniform2f(renderer.resolution, gl.canvas.width, gl.canvas.height);
+            gl.uniform2f(renderer.resolution, gl.canvas.width / window.devicePixelRatio, gl.canvas.height / window.devicePixelRatio);
 
             if (!viz.symbol.default) {
                 const textureId = viz.symbolShader.textureIds.get(viz);
