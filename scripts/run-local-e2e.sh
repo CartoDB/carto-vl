a89f4eab9defe316c56ac04da4208c2483f06db3
Raul Ochoa <rochoaf@gmail.com>
2018-10-23 18:01:53 +0000

    Pass all arguments from parent script
    This allows to run something like "yarn test:e2e --grep wadus"

--- scripts/run-local-e2e.sh
+++ scripts/run-local-e2e.sh
@@ -11,6 +11,6 @@ TESTS_HOSTNAME=localhost.localhost.lan
 valid_hostname() {
     node -e "const hostname = '${TESTS_HOSTNAME}'; require('dns').lookup(hostname, (err, address) => { if (err) { console.error('Could not resolve hostname \"%s\", reason %s', hostname, err.message); return process.exit(1); } })"
 }
-valid_hostname && yarn build:dev && yarn wait-on "http://${TESTS_HOSTNAME}:8181" && mocha test/acceptance/e2e.test.js --timeout 15000;
+valid_hostname && yarn build:dev && yarn wait-on "http://${TESTS_HOSTNAME}:8181" && mocha test/acceptance/e2e.test.js --timeout 15000 "$@";
 
 # docker kill `cat .docker.run`
