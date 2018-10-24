415a87d751dbc619527534f346b0ab85a624d5f1
David M <dmanzanares@cartodb.com>
2018-10-24 13:12:36 +0200

    Improve top render test to catch regression

--- test/integration/render/scenarios/regression/cartocolor-top/scenario.js
+++ test/integration/render/scenarios/regression/cartocolor-top/scenario.js
@@ -4,7 +4,12 @@ const map = new CartoMap({
 });
 
 const source = new carto.source.GeoJSON(sources['points']);
-const viz = new carto.Viz('color: ramp(top($cat, @num), PRISM), width: 50, @num: 2');
+const viz = new carto.Viz(`
+    color: ramp(top(@prop, @num), PRISM)
+    width: 50
+    @prop: $cat
+    @num: 2
+`);
 const layer = new carto.Layer('layer', source, viz);
 
 viz.variables.num.blendTo(1, 0);
