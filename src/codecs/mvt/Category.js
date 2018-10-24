237296e77a23c5c23ce3616ab34331f1de3e0e7e
Javier Goizueta <jgoizueta@gmail.com>
2018-10-25 00:19:26 +0200

    Fix

--- src/codecs/mvt/Category.js
+++ src/codecs/mvt/Category.js
@@ -9,7 +9,7 @@ export default class MVTCategoryCodec extends CategoryCodec {
             // but for the generic MVT sources we don't support multiple source properties per base property (e.g. aggregations)
             // so it would suffice to keep the property name in the Codec class.
             // For more general solutions we'd need to provide the source property name as an argument to this method.
-            throw new CartoRuntimeError(`${crt.MVT} MVT decoding error. Metadata property is of type 'number' but the MVT tile contained a feature property of type '${typeof propertyValue}': '${propertyValue}'`);
+            throw new CartoRuntimeError(`${crt.MVT} MVT decoding error. Metadata property is of type 'category' but the MVT tile contained a feature property of type '${typeof propertyValue}': '${propertyValue}'`);
         }
         return super.sourceToInternal(propertyValue);
     }
