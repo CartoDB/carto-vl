0247d3bd730869d95f5f774044f559af54dae3c0
David M <dmanzanares@cartodb.com>
2018-10-24 12:00:49 +0200

    Remove console.log

--- src/renderer/viz/expressions/Image.js
+++ src/renderer/viz/expressions/Image.js
@@ -109,7 +109,6 @@ function _getCanvasFromImage (img) {
     const width = img.width / max * CANVAS_SIZE;
     const height = img.height / max * CANVAS_SIZE;
 
-    console.log(width, height);
     ctx.drawImage(img, (CANVAS_SIZE - width) / 2, (CANVAS_SIZE - height) / 2, width, height);
 
     return canvas;
