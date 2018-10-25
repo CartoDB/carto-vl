a3579f5718c36ffe85ac3c8684c18ced57412461
Javier Goizueta <jgoizueta@gmail.com>
2018-10-25 13:08:17 +0200

    Handle null/NaN in number codec

--- src/codecs/Number.js
+++ src/codecs/Number.js
@@ -1,4 +1,26 @@
 import IdentityCodec from './Identity';
+import { FP32_DESIGNATED_NULL_VALUE } from '../renderer/viz/expressions/constants';
 
 export default class NumberCodec extends IdentityCodec {
+
+    sourceToInternal (propertyValue) {
+        if (isNaN(propertyValue) || propertyValue == null) {
+            propertyValue = FP32_DESIGNATED_NULL_VALUE;
+        }
+        return [propertyValue];
+    }
+
+    internalToExternal (value) {
+        if (value === FP32_DESIGNATED_NULL_VALUE) {
+            value = null;
+        }
+        return value;
+    }
+
+    externalToInternal (value) {
+        if (value === null) {
+            value = FP32_DESIGNATED_NULL_VALUE;
+        }
+        return [value];
+    }
 }
