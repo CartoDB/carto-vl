f2200b1dc0fddcb43508097a34e4f2dda6191c78
Raul Ochoa <rochoaf@gmail.com>
2018-10-25 13:18:42 +0000

    Going red: validate our test fails when there is an assertion failing

---
+++ test/acceptance/e2e/skip-render-check/legend/scenario.js
@@ -0,0 +1,28 @@
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: { version: 8, sources: {}, layers: [] },
+    center: [-74, 40.7],
+    zoom: 12
+});
+
+carto.setDefaultAuth({
+    user: 'localhost',
+    apiKey: '1234'
+});
+carto.setDefaultConfig({
+    serverURL: 'http://127.0.0.1:8181/user/{user}'
+});
+const buckets = 5;
+const source = new carto.source.Dataset('mnmappluto');
+const viz = new carto.Viz(`
+    color: ramp(top($landuse, ${buckets}), Bold)
+    strokeWidth: 0
+`);
+const layer = new carto.Layer('layer', source, viz);
+
+layer.addTo(map);
+layer.on('loaded', async function () {
+    const legend = viz.color.getLegendData();
+    assert(legend.data.length === 0, 'Unexpected number of buckets');
+    window.loaded = true;
+});
