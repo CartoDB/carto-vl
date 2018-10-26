7979815a96a13b795ecf686f86dd738f665f77a9
David Manzanares <dmanzanares@cartodb.com>
2018-10-26 13:12:31 +0200

    Fix shader `textureIds` to make it per expression

--- src/Viz.js
+++ src/Viz.js
@@ -323,37 +323,37 @@ export default class Viz {
         this._polygonShader = null;
     }
 
-    get colorShader () {
+    get colorMetaShader () {
         return this._compileShader('colorShader', shaders.styler.colorShaderGLSL, { color: this.color });
     }
-    get widthShader () {
+    get widthMetaShader () {
         return this._compileShader('widthShader', shaders.styler.widthShaderGLSL, { width: this.width });
     }
-    get strokeColorShader () {
+    get strokeColorMetaShader () {
         return this._compileShader('strokeColorShader', shaders.styler.colorShaderGLSL, { color: this.strokeColor });
     }
-    get strokeWidthShader () {
+    get strokeWidthMetaShader () {
         return this._compileShader('strokeWidthShader', shaders.styler.widthShaderGLSL, { width: this.strokeWidth });
     }
-    get filterShader () {
+    get filterMetaShader () {
         return this._compileShader('filterShader', shaders.styler.filterShaderGLSL, { filter: this.filter });
     }
-    get symbolShader () {
+    get symbolMetaShader () {
         return this._compileShader('symbolShader', shaders.symbolizer.symbolShaderGLSL, {
             symbol: this.symbol,
             symbolPlacement: this.symbolPlacement,
             transform: this.transform
         });
     }
-    get pointShader () {
+    get pointMetaShader () {
         return this._compileShader('pointShader', { vertexShader: pointVertexShaderGLSL, fragmentShader: pointFragmentShaderGLSL },
             { transform: this.transform });
     }
-    get lineShader () {
+    get lineMetaShader () {
         return this._compileShader('lineShader', { vertexShader: lineVertexShaderGLSL, fragmentShader: lineFragmentShaderGLSL },
             { transform: this.transform });
     }
-    get polygonShader () {
+    get polygonMetaShader () {
         return this._compileShader('polygonShader', { vertexShader: polygonVertexShaderGLSL, fragmentShader: polygonFragmentShaderGLSL },
             { transform: this.transform });
     }
