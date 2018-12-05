d2de9eea0a192bc6ed53858effe00e8086c14ece
Víctor Velarde <victor@carto.com>
2018-12-05 18:49:15 +0100

    Refactor to explicit build on prepublishOnly

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,8 +6,6 @@ echo "Ready to publish CARTO VL version $VERSION"
 echo "Has the version number been bumped?"
 read -n1 -r -p "Press Ctrl+C to cancel, or any other key to continue." key
 
-yarn build
-
 echo "Uploading to CDN..."
 
 node scripts/cdn/publish.js
