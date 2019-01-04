c1a1faa5e6b26bec05d1acb1f96ae051eaba9fcd
Víctor Velarde <victor@carto.com>
2019-01-04 20:21:37 +0100

    Use feature pieces (n dataframes) for getCentroid in viewportFeatures

--- src/renderer/viz/expressions/aggregation/viewport/ViewportAggCalculator.js
+++ src/renderer/viz/expressions/aggregation/viewport/ViewportAggCalculator.js
@@ -1,5 +1,6 @@
 
 import { FILTERING_THRESHOLD } from '../../../../Renderer';
+import ViewportFeatures from '../../viewportFeatures';
 
 /**
  * Run all viewport aggregation functions over the visible features in the renderLayer
@@ -48,14 +49,24 @@ function _reset (viewportExpressions, renderLayer) {
 function _runInActiveDataframes (viewportExpressions, renderLayer) {
     const processedFeaturesIDs = new Set(); // same feature can belong to multiple dataframes
     const dataframes = renderLayer.getActiveDataframes();
+    const viz = renderLayer.viz;
+
     dataframes.forEach(dataframe => {
-        _runInDataframe(renderLayer.viz, viewportExpressions, dataframe, processedFeaturesIDs);
+        _runInDataframe(viz, viewportExpressions, dataframe, processedFeaturesIDs);
     });
+    if (processedFeaturesIDs.size === 0) {
+        return;
+    }
+
+    const viewportFeaturesExpressions = viewportExpressions.filter(exp => exp.isA(ViewportFeatures));
+    if (viewportFeaturesExpressions.length > 0) {
+        _runViewportFeatures(viewportFeaturesExpressions, renderLayer, processedFeaturesIDs);
+    }
 }
 
 /**
  * Run viewport aggregations in the dataframe. It excludes features:
- *    - already accumlated in other dataframes
+ *    - already accumulated in other dataframes
  *    - outside the viewport
  *    - filtered out
  */
@@ -65,7 +76,7 @@ function _runInDataframe (viz, viewportExpressions, dataframe, processedFeatures
 
         const featureAlreadyAccumulated = processedFeaturesIDs.has(featureId);
         if (featureAlreadyAccumulated) {
-            continue;
+            continue; // This is correct for viewportExpressions related to 'alphanumeric' properties (not geometry-related)
         }
 
         const featureOutsideViewport = !dataframe.inViewport(i);
@@ -82,7 +93,44 @@ function _runInDataframe (viz, viewportExpressions, dataframe, processedFeatures
             continue;
         }
 
-        // not a filtered feature, so pass the rawFeature to all viewport aggregations
+        // not a filtered feature, so pass the rawFeature to viewport expressions
         viewportExpressions.forEach(expr => expr.accumViewportAgg(feature));
+    }
+}
+
+function _runViewportFeatures (viewportFeaturesExpressions, renderLayer, processedFeaturesIDs) {
+    const dataframes = renderLayer.getActiveDataframes();
+    const idProperty = renderLayer.viz.metadata.idProperty;
+
+    // Reset previous expressions with (possibly partial) features
+    viewportFeaturesExpressions.forEach(expr => expr._resetViewportAgg(null, renderLayer));
+
+    // Accumulate considering the featurePieces
+    processedFeaturesIDs.forEach((featureId) => {
+        const featurePieces = dataframes.reduce((result, dataframe) => {
+            _addPartialFeatureIfExistsIn(dataframe, featureId, idProperty, result);
+            return result;
+        }, []);
+        viewportFeaturesExpressions.forEach(expr => expr.accumViewportAgg(featurePieces));
+    });
+}
+
+/**
+ * Add the feature with featureId in the dataframe, if is included there.
+ * It could be just a, geometrically speaking, 'partial feature' (a piece of it)
+ */
+function _addPartialFeatureIfExistsIn (dataframe, featureId, idProperty, result) {
+    let partialFeature;
+
+    for (let i = 0; i < dataframe.numFeatures; i++) {
+        const currentId = dataframe.properties[idProperty][i];
+        if (currentId === featureId) {
+            partialFeature = dataframe.getFeature(i);
+            break;
+        }
+    }
+
+    if (partialFeature !== undefined) {
+        result.push(partialFeature);
     }
 }
