2b1efb5f6c220440d847a41dfaec63a464818526
Víctor Velarde <victor@carto.com>
2019-01-07 22:05:09 +0100

    Improved centroid for features with multiple pieces with area weighting

--- src/interactivity/commonFeature.js
+++ src/interactivity/commonFeature.js
@@ -1,5 +1,18 @@
 
-import { average } from '../renderer/viz/expressions/stats';
+function _getGeomPropertiesFromPieces (pieces) {
+    const geomPropertiesFromPieces = pieces.map(piece => {
+        const [x, y] = piece.getCentroid();
+        const aabb = piece._rawFeature._dataframe._aabb[piece._rawFeature._index];
+        return { centroid: { x, y }, aabb };
+    });
+
+    const geomProperties = geomPropertiesFromPieces.filter(piece => {
+        const { x, y } = piece.centroid;
+        return !(isNaN(x) || isNaN(y));
+    });
+
+    return geomProperties;
+}
 
 /**
  * Get a 'compound Feature'. It is a Feature created from a set of 'feature' pieces
@@ -9,18 +22,20 @@ export function getCompoundFeature (featurePieces) {
     const exemplar = featurePieces[0];
     if (featurePieces.length === 1) return exemplar;
 
-    // Unify geometry-related properties, into an 'exemplar'.
-    const allCentroids = featurePieces.map(piece => piece.getCentroid());
-    const centroids = allCentroids.filter(c => {
-        return !(isNaN(c[0]) || isNaN(c[1]));
-    });
-
+    const geomProperties = _getGeomPropertiesFromPieces(featurePieces);
     Object.defineProperty(exemplar, 'getCentroid', {
         get: function () {
             const getCentroid = () => {
-                const avgX = average(centroids.map(c => c[0]));
-                const avgY = average(centroids.map(c => c[1]));
-                return [avgX, avgY]; // vs this._rawFeature._dataframe.getCentroid(this._rawFeature._index);
+                // average of centroids ponderated by aabb size
+                const [weightedXs, weightedYs, totalSize] = geomProperties.reduce((accumulator, currentValue) => {
+                    const { minx, miny, maxx, maxy } = currentValue.aabb;
+                    const size = (maxx - minx) * (maxy - miny);
+                    const pX = currentValue.centroid.x * size;
+                    const pY = currentValue.centroid.y * size;
+                    return [accumulator[0] + pX, accumulator[1] + pY, accumulator[2] + size];
+                }, [0, 0, 0]);
+
+                return [weightedXs / totalSize, weightedYs / totalSize];
             };
             return getCentroid;
         }
