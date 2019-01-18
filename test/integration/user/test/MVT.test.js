47ba6cbabd216990a9d26304034b49d540df759e
Víctor Velarde <victor@carto.com>
2019-01-18 18:01:27 +0100

    Remove unstable integration user test on MVT Interactivity

--- test/integration/user/test/MVT.test.js
+++ test/integration/user/test/MVT.test.js
@@ -35,45 +35,6 @@ describe('Layer', () => {
         });
     });
 
-    // It passes fine on `test:user:watchc` but not with `test:user:min`
-    xdescribe('regression with Interactivity (due to multiple feature pieces search)', () => {
-        it('should work fine in a polygon Layer with custom featureId', done => {
-            const metadata = { ...METADATA, idProperty: 'winner' };
-            source = new carto.source.MVT(MVT_FILE, metadata);
-            viz = new carto.Viz(`
-                @f_cartodb_id: $cartodb_id
-                @f_winner: $winner
-
-                @v_features: viewportFeatures($winner)
-                strokeWidth: 1
-            `);
-            layer = new carto.Layer('my_mvt_layer', source, viz);
-            const interactivity = new carto.Interactivity(layer);
-            layer.addTo(map);
-
-            interactivity.on('featureClick', event => {
-                expect(event.features.length).toBeGreaterThan(0);
-
-                const aFeature = event.features[0];
-                expect(aFeature.layerId).toEqual('my_mvt_layer');
-
-                expect(aFeature.id).toBeDefined();
-                expect(aFeature.variables.f_cartodb_id).toBeDefined();
-                expect(aFeature.variables.f_winner).toBeDefined();
-
-                done();
-            });
-
-            onLoaded(() => {
-                util.simulateClick({ lng: 0, lat: 50 });
-            });
-        });
-    });
-
-    function onLoaded (callback) {
-        layer.on('loaded', callback);
-    }
-
     afterEach(() => {
         map.remove();
         document.body.removeChild(div);
