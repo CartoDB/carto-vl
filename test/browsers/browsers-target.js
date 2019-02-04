31c8afffd0567bc62f4b96f9abb4f359b74cde86
Víctor Velarde <victor@carto.com>
2019-02-04 13:24:35 +0100

    Add browsers to target list, using latest

--- test/browsers/browsers-target.js
+++ test/browsers/browsers-target.js
@@ -1,7 +1,15 @@
 // See https://wiki.saucelabs.com/display/DOCS/Platform+Configurator# for a list of supported options
 const browsers = [
-    { 'platform': 'Windows 10', 'browserName': 'chrome', 'version': '71' },
-    { 'platform': 'Windows 10', 'browserName': 'MicrosoftEdge', 'version': '18.17763' }
+    { 'platform': 'Windows 10', 'browserName': 'chrome', 'version': 'latest' },
+    { 'platform': 'Windows 10', 'browserName': 'firefox', 'version': 'latest' },
+    { 'platform': 'Windows 10', 'browserName': 'MicrosoftEdge', 'version': 'latest' },
+
+    { 'platform': 'macOS 10.14', 'browserName': 'chrome', 'version': 'latest' },
+    { 'platform': 'macOS 10.14', 'browserName': 'firefox', 'version': 'latest' },
+    { 'platform': 'macOS 10.14', 'browserName': 'safari', 'version': 'latest' }
+
+    // { 'platform': 'Linux', 'browserName': 'chrome', 'version': 'latest' },
+    // { 'platform': 'Linux', 'browserName': 'firefox', 'version': 'latest' },
 ];
 
 module.exports = browsers;
