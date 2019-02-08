2911971d9d07df757d72c88870c92f4dfc45ede0
Víctor Velarde <victor@carto.com>
2019-02-08 12:06:39 +0100

    Just Windows 10 tests

--- test/browsers/browsers-target.js
+++ test/browsers/browsers-target.js
@@ -2,11 +2,11 @@
 const browsers = [
     { 'platform': 'Windows 10', 'browserName': 'chrome', 'version': 'latest' },
     { 'platform': 'Windows 10', 'browserName': 'firefox', 'version': 'latest' },
-    { 'platform': 'Windows 10', 'browserName': 'MicrosoftEdge', 'version': 'latest' },
+    { 'platform': 'Windows 10', 'browserName': 'MicrosoftEdge', 'version': 'latest' }
 
-    { 'platform': 'macOS 10.14', 'browserName': 'chrome', 'version': 'latest' },
-    { 'platform': 'macOS 10.14', 'browserName': 'firefox', 'version': 'latest' },
-    { 'platform': 'macOS 10.14', 'browserName': 'safari', 'version': 'latest' }
+    // { 'platform': 'macOS 10.14', 'browserName': 'chrome', 'version': 'latest' },
+    // { 'platform': 'macOS 10.14', 'browserName': 'firefox', 'version': 'latest' },
+    // { 'platform': 'macOS 10.14', 'browserName': 'safari', 'version': 'latest' }
 
     // { 'platform': 'Linux', 'browserName': 'chrome', 'version': 'latest' },
     // { 'platform': 'Linux', 'browserName': 'firefox', 'version': 'latest' },
