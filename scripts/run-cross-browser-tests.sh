8ce516d1af346f46128c6610f0a3c2abe8fb44bd
Víctor Velarde <victor@carto.com>
2019-02-08 12:06:28 +0100

    Preventively kill localhost:8080 web server

--- scripts/run-cross-browser-tests.sh
+++ scripts/run-cross-browser-tests.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 lsof -ti:8080 | xargs kill # kill http_server
 yarn serve &
-HTTP_SERVER_PID=$!
 yarn wait-on "http://localhost:8080" && mocha test/browsers/browsers.test.js;
-kill $HTTP_SERVER_PID
