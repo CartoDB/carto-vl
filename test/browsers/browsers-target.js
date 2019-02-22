98040409a68359a0fc91e513ae4128ab026a424b
alejandraarri <alejandraarri@carto.com>
2019-02-22 12:01:43 +0100

    Improve the target browsers list

--- test/browsers/browsers-target.js
+++ test/browsers/browsers-target.js
@@ -4,12 +4,10 @@ const browsers = [
     { 'platform': 'Windows 10', 'browserName': 'firefox', 'version': 'latest' },
     { 'platform': 'Windows 10', 'browserName': 'MicrosoftEdge', 'version': 'latest' }
 
+    // TODO Fix SauceLabs tests to run on macOS #1272
     // { 'platform': 'macOS 10.14', 'browserName': 'chrome', 'version': 'latest' },
     // { 'platform': 'macOS 10.14', 'browserName': 'firefox', 'version': 'latest' },
     // { 'platform': 'macOS 10.14', 'browserName': 'safari', 'version': 'latest' }
-
-    // { 'platform': 'Linux', 'browserName': 'chrome', 'version': 'latest' },
-    // { 'platform': 'Linux', 'browserName': 'firefox', 'version': 'latest' },
 ];
 
 module.exports = browsers;
