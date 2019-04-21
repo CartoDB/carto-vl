bea90e4aca64308ca499f9804e53c02c7bef4b36
Víctor Velarde Gutierrez <victor.velarde@gmail.com>
2019-04-21 13:00:20 +0200

    Geometry type conversion for geojson dataframes

--- src/sources/geojson/GeoJSONGeometryType.js
+++ src/sources/geojson/GeoJSONGeometryType.js
@@ -1,3 +1,5 @@
+import { GEOMETRY_TYPE } from '../../utils/geometry';
+
 /**
  * Geometry types available at GeoJSON format
  */
@@ -9,4 +11,22 @@ const GeoJSONGeometryType = Object.freeze({
     MULTI_POLYGON: 'MultiPolygon'
 });
 
-export default GeoJSONGeometryType;
+/**
+ * Dataframe geometry type corresponding to every GeoJSON geometry type
+ */
+function dataframeGeometryType (geojsonGeometryType) {
+    switch (geojsonGeometryType) {
+        case GeoJSONGeometryType.POINT:
+            return GEOMETRY_TYPE.POINT;
+        case GeoJSONGeometryType.LINE_STRING:
+        case GeoJSONGeometryType.MULTI_LINE_STRING:
+            return GEOMETRY_TYPE.LINE;
+        case GeoJSONGeometryType.POLYGON:
+        case GeoJSONGeometryType.MULTI_POLYGON:
+            return GEOMETRY_TYPE.POLYGON;
+        default:
+            return '';
+    }
+}
+
+export { GeoJSONGeometryType, dataframeGeometryType };
