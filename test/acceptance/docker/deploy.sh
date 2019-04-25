0faa14c518808ee62941c40dcaf58f742aa3f819
elenatorro <elenatorro@gmail.com>
2019-04-25 12:47:42 +0200

    Use windshaft 6.5.1 for testing

--- test/acceptance/docker/deploy.sh
+++ test/acceptance/docker/deploy.sh
@@ -5,8 +5,8 @@ git clone https://github.com/CartoDB/Windshaft-cartodb.git ;
 cp /srv/config/environments/test.js Windshaft-cartodb/config/environments/development.js
 cd ./Windshaft-cartodb/
 
-git checkout master
-git pull origin master
+git checkout 6.5.1
+git pull origin 6.5.1
 
 npm ci
 
