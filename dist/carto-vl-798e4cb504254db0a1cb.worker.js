bf776267359617a3cbcaa22533723548a3c3a801
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2020-06-20 09:30:39 +0200

    Update dist

--- dist/carto-vl-798e4cb504254db0a1cb.worker.js
+++ dist/carto-vl-798e4cb504254db0a1cb.worker.js
@@ -19393,6 +19393,282 @@ class BigQueryClient {
 
 /***/ }),
 
+/***/ "./src/sources/bigquery/BQQuadkey.js":
+/*!*******************************************!*\
+  !*** ./src/sources/bigquery/BQQuadkey.js ***!
+  \*******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return GilbertPartition; });
+/* eslint-disable */
+
+const PARTITION_UNPARTITIONED = 0;
+
+/**
+ * Assigns partitions to zoom levels given the number of partitions and zoom ranges available
+ * Partitions always start at 1 (0 is used for outside levels) and end at the **partitions** parameter
+ *
+ * On the default situation (zmin = 0) we assign to the max zoom level, 3/4ths of the available partitions
+ * since approximately 75% of the tiles are always found in the highest zoom. Then, we assign 75%
+ * of the **remaining** space to the zoom level right below it, 75%(75%(75%)) to the next one, and so on.
+ *
+ * When (zmin != 0) we divide the "saved" partition space in the same proportion as the full partition table
+ */
+function partition_range_by_zoom (partition_count, { z, zmin, zmax }) {
+    if (z < 0 || zmin < 0 || zmax < 0 || z < zmin || z > zmax || partition_count < 1)
+        return { min: PARTITION_UNPARTITIONED, max: PARTITION_UNPARTITIONED };
+
+    const global_start = 1;
+    const global_end = partition_count;
+
+    if (zmin == zmax || global_start == global_end) {
+        return { min: global_start, max: global_end };
+    }
+
+    const zoom_divisor = (Math.pow(4, zmax - z + 1));
+    let saved_space = 0;
+    let saved_space_used_by_previous = 0;
+    let saved_space_used_by_z = 0;
+    if (zmin !== 0) {
+        /* We calculate the offsets of the saved space */
+        const zmin_divisor = Math.pow(4, zmax - zmin + 1);
+        saved_space = global_end / zmin_divisor;
+        saved_space_used_by_previous = saved_space * (Math.pow(4, z - zmin) - 1) / zmin_divisor;
+        saved_space_used_by_z = saved_space * 3 / zoom_divisor;
+    }
+
+    const z_partitions = global_end * 3 / zoom_divisor;
+    let partition_start = global_end / zoom_divisor - (saved_space - saved_space_used_by_previous);
+    const partition_end = z == zmax ? global_end - global_start : partition_start + z_partitions + saved_space_used_by_z;
+    /* We round the start (instead of using floor) to give the lower zoom levels more partitions as 
+     * they tend to be overcrowded, especially in rectangles with a great difference between height and width
+     */
+    partition_start = Math.round(partition_start);
+
+    return {
+        min: partition_start + global_start,
+        max: Math.min(global_end, Math.max(Math.floor(partition_end), partition_start) + global_start)
+    };
+}
+
+/* Finds the position of `target` inside the Gilbert curve defined by the rectangle:
+ * cursor: Current position (start)
+ * vector_main: Main direction of movement.
+ *              It should move only on one axis, and it can be positive or negative.
+ * vector_sec: Secondary direction of movement. Orthogonal to and smaller than vector_main.
+ *
+ * This is currently recursive, so the value is being add up inside target.counter
+ */
+function gilbert2d(cursor = { x, y },
+    vector_main = { x, y },
+    vector_sec = { x, y },
+    target = { counter, x, y }) {
+    /* Vector direction of movement (-1, 0 or +1) */
+    const main_dirx = Math.sign(vector_main.x);
+    const main_diry = Math.sign(vector_main.y);
+    const sec_dirx = Math.sign(vector_sec.x);
+    const sec_diry = Math.sign(vector_sec.y);
+
+    const xmin = Math.min(cursor.x, cursor.x + vector_main.x - main_dirx + vector_sec.x - sec_dirx);
+    const xmax = Math.max(cursor.x, cursor.x + vector_main.x - main_dirx + vector_sec.x - sec_dirx);
+    const ymin = Math.min(cursor.y, cursor.y + vector_main.y - main_diry + vector_sec.y - sec_diry);
+    const ymax = Math.max(cursor.y, cursor.y + vector_main.y - main_diry + vector_sec.y - sec_diry);
+    if (target.x < xmin || target.x > xmax || target.y < ymin || target.y > ymax) {
+        /* The tile we are looking for is not insde this rectangle. Add up its size and do a fast exit */
+        target.counter += (xmax - xmin + 1) * (ymax - ymin + 1);
+        return -1;
+    }
+
+    /* We can calculate the magnitude of the vectors this way because one of the coordinates
+     * will always be 0 */
+    const vector_main_magnitude = Math.abs(vector_main.x + vector_main.y);
+    const vector_sec_magnitude = Math.abs(vector_sec.x + vector_sec.y);
+
+    if (vector_main_magnitude == 1 || vector_sec_magnitude == 1) {
+        /* We have either one row or one column. Either way, the position is the distance between
+         * the current point and the target */
+        target.counter += Math.abs((cursor.x - target.x) + (cursor.y - target.y));
+        return target.counter;
+    }
+
+    const vector_main_half = {
+        x: Math.floor(vector_main.x / 2),
+        y: Math.floor(vector_main.y / 2)
+    };
+
+    if (2 * vector_main_magnitude > 3 * vector_sec_magnitude) {
+        const vector_main_half_magnitude = Math.abs(vector_main_half.x + vector_main_half.y);
+        if ((vector_main_half_magnitude % 2) && (vector_main_magnitude > 2)) {
+            /* Prefer even steps */
+            vector_main_half.x += main_dirx;
+            vector_main_half.y += main_diry;
+        }
+
+        /* We split the main vector into 2 parts */
+        const first = gilbert2d({ x: cursor.x, y: cursor.y },
+            vector_main_half,
+            vector_sec,
+            target);
+        if (first !== -1)
+            return first;
+        return gilbert2d({ x: cursor.x + vector_main_half.x, y: cursor.y + vector_main_half.y },
+            { x: vector_main.x - vector_main_half.x, y: vector_main.y - vector_main_half.y },
+            vector_sec,
+            target);
+    } else {
+        const vector_sec_half = {
+            x: Math.floor(vector_sec.x / 2),
+            y: Math.floor(vector_sec.y / 2)
+        };
+        const vector_sec_half_magnitude = Math.abs(vector_sec_half.x + vector_sec_half.y);
+        if ((vector_sec_half_magnitude % 2) && (vector_sec_magnitude > 2)) {
+            /* Prefer even steps */
+            vector_sec_half.x += sec_dirx;
+            vector_sec_half.y += sec_diry;
+        }
+
+        /* We split the whole rectangle into 3 parts */
+
+        /* Bottom half left (flipped so it finishes on top of the starting point) */
+        const first = gilbert2d({ x: cursor.x, y: cursor.y },
+            vector_sec_half,
+            vector_main_half,
+            target);
+        if (first !== -1)
+            return first;
+
+        /* Top (left to right) */
+        const second = gilbert2d({ x: cursor.x + vector_sec_half.x, y: cursor.y + vector_sec_half.y },
+            vector_main,
+            { x: vector_sec.x - vector_sec_half.x, y: vector_sec.y - vector_sec_half.y },
+            target);
+        if (second !== -1)
+            return second;
+
+        /* Bottom half right */
+        return gilbert2d({
+            x: cursor.x + (vector_main.x - main_dirx) + (vector_sec_half.x - sec_dirx),
+            y: cursor.y + (vector_main.y - main_diry) + (vector_sec_half.y - sec_diry)
+        },
+            { x: - vector_sec_half.x, y: - vector_sec_half.y },
+            { x: - (vector_main.x - vector_main_half.x), y: - (vector_main.y - vector_main_half.y) },
+            target);
+    }
+}
+
+/**
+ * Given a rectangle coordinate (x : [0, width) ^ y : [ 0 .. height)) and its height and width,
+ * it returns the position of the given coordinate in the Gilbert curve filling the rectangle
+ * The result will be an integer number n : [0, (width * height) - 1]
+ */
+function gilbert2d_tile(target_tile = { x, y }, width, height) {
+    if (width < 1 || height < 1)
+        throw `Invalid Gilbert rectangle. Got: Width (${width}). Height: (${height})`;
+    const tile = { x: target_tile.x, y: target_tile.y, counter: 0 };
+    const start_point = { x: 0, y: 0 };
+
+    let main_vector = { x: 0, y: height };
+    let secondary_vector = { x: width, y: 0 };
+    if (width > height) {
+        main_vector = { x: width, y: 0 };
+        secondary_vector = { x: 0, y: height };
+    }
+    return gilbert2d(start_point, main_vector, secondary_vector, tile);
+}
+
+class GilbertPartition {
+    /**
+     * partition_count: Number of partitions assigned for the whole dataset
+     * z_range: Range of accepted zoom levels (e.g: { zmin: 0, zmax: 15 })
+     * zmax_bbox: Bounding box of the dataset for the maximum accepted zoom level (i.e. z_range.zmax)
+     *            These are tile coordinates (x, y) not geography coordinates
+     */
+    constructor(partition_count,
+        z_range = { zmin, zmax },
+        zmax_bbox = { xmin, xmax, ymin, ymax }) {
+        this.partition_count = parseInt(partition_count);
+        this.z_range = {
+            zmin: parseInt(z_range.zmin),
+            zmax: parseInt(z_range.zmax)
+        };
+        this.zmax_bbox = {
+            xmin: parseInt(zmax_bbox.xmin),
+            xmax: parseInt(zmax_bbox.xmax),
+            ymin: parseInt(zmax_bbox.ymin),
+            ymax: parseInt(zmax_bbox.ymax)
+        };
+
+        if (!partition_count || partition_count < 1)
+            throw `Unexpected partition count. Expected a positive integer. Got ${partition_count}`;
+        if (!this.z_range)
+            throw `Missing z_range`;
+        if (isNaN(this.z_range.zmin) || isNaN(this.z_range.zmax))
+            throw `Invalid z_range. Both zmin and zmax need to be defined. Got ${JSON.stringify(z_range)}`;
+        if (this.z_range.zmin > this.z_range.zmax || this.z_range.zmin < 0)
+            throw `Invalid z_range received. Got (${JSON.stringify(z_range)})`;
+        if (!this.zmax_bbox)
+            throw `Missing zmax_bbox`;
+        if (isNaN(this.zmax_bbox.xmin) || isNaN(this.zmax_bbox.xmax) ||
+            isNaN(this.zmax_bbox.ymin) || isNaN(this.zmax_bbox.ymax))
+            throw `Invalid zmax_bbox. All xmin, xmax, ymin and ymax need to be defined. Got (${JSON.stringify(zmax_bbox)})`;
+        if (this.zmax_bbox.xmin > this.zmax_bbox.xmax || this.zmax_bbox.xmin < 0 ||
+            this.zmax_bbox.ymin > this.zmax_bbox.ymax || this.zmax_bbox.ymin < 0)
+            throw `Invalid bbox received. Got (${JSON.stringify(zmax_bbox)})`;
+    }
+
+    getPartition(tile = { z: 0, x: 0, y: 0 }) {
+        const rxmin = this.zmax_bbox.xmin >> (this.z_range.zmax - tile.z);
+        const rxmax = this.zmax_bbox.xmax >> (this.z_range.zmax - tile.z);
+        const rymin = this.zmax_bbox.ymin >> (this.z_range.zmax - tile.z);
+        const rymax = this.zmax_bbox.ymax >> (this.z_range.zmax - tile.z);
+        if (tile.z < this.z_range.zmin || tile.z > this.z_range.zmax ||
+            tile.x < rxmin || tile.x > rxmax ||
+            tile.y < rymin || tile.y > rymax) {
+            return PARTITION_UNPARTITIONED;
+        }
+
+        const partition_range = partition_range_by_zoom(this.partition_count,
+            { z: tile.z, zmin: this.z_range.zmin, zmax: this.z_range.zmax });
+        const usable_partitions = partition_range.max - partition_range.min + 1;
+        if (usable_partitions == 1) {
+            return partition_range.min;
+        }
+
+        /* We switch to a rectangle starting at 0, 0 so it's easier to reason about it
+         * The rectangle goes from (0, 0) to (rwidth - 1, rheight - 1)
+         */
+        const x = tile.x - rxmin;
+        const y = tile.y - rymin;
+        const rwidth = rxmax - rxmin + 1;
+        const rheight = rymax - rymin + 1;
+        const rectangle_size = rwidth * rheight;
+
+        /* We need to divide this **rectangle** into equal sized areas, so what we do is to create a
+         * Hilbert curve and then divide this 1d line into the necessary parts.
+         *
+         * If this was a perfect square, we would interleave bits from x and y, but in a rectangle
+         * that would leave many empty spaces, so the partitions would be irregular (or some even empty)
+         *
+         * Instead we use a Generalized Hilbert curve based on https://github.com/jakubcerveny/gilbert
+         * by Jakub Červený
+         *
+         */
+        const gilbert_position = gilbert2d_tile({ x: x, y: y }, rwidth, rheight);
+        if (gilbert_position === -1 || gilbert_position >= rwidth * rheight) {
+            throw `Unexpected gilbert partition (${gilbert_position}) for tile ${tile.z}/${tile.x}/${tile.y} ` +
+            `=> ${x}/${y} : ${rwidth}, ${rheight})`;
+        }
+
+        /* The gilbert_position goes from 0 to rectangle_size - 1 */
+        return partition_range.min + Math.floor(usable_partitions * gilbert_position / rectangle_size);
+    }
+}
+
+/***/ }),
+
 /***/ "./src/sources/bigquery/BQTilesetClient.js":
 /*!*************************************************!*\
   !*** ./src/sources/bigquery/BQTilesetClient.js ***!
@@ -19404,13 +19680,18 @@ class BigQueryClient {
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return BigQueryTilesetClient; });
 /* harmony import */ var _BQClient__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./BQClient */ "./src/sources/bigquery/BQClient.js");
-/* harmony import */ var base64_arraybuffer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! base64-arraybuffer */ "./node_modules/base64-arraybuffer/lib/base64-arraybuffer.js");
-/* harmony import */ var base64_arraybuffer__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(base64_arraybuffer__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var pako__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! pako */ "./node_modules/pako/index.js");
-/* harmony import */ var pako__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(pako__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _BQQuadkey__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./BQQuadkey */ "./src/sources/bigquery/BQQuadkey.js");
+/* harmony import */ var base64_arraybuffer__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! base64-arraybuffer */ "./node_modules/base64-arraybuffer/lib/base64-arraybuffer.js");
+/* harmony import */ var base64_arraybuffer__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(base64_arraybuffer__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var pako__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! pako */ "./node_modules/pako/index.js");
+/* harmony import */ var pako__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(pako__WEBPACK_IMPORTED_MODULE_3__);
 
 
 
+
+
+// Singleton
+let _partitioner = null;
 
 class BigQueryTilesetClient {
     constructor (projectId, token) {
@@ -19440,8 +19721,12 @@ class BigQueryTilesetClient {
     }
 
     async fetchTiles (tiles, dataset, tileset, tilesetMetadata) {
-        const quadKeyZoom = parseInt(tilesetMetadata.carto_quadkey_zoom);
-        const parentQuadkeys = getParentIntegerQuadkeysFromTiles(tiles, quadKeyZoom);
+        if (!_partitioner) {
+            const params = JSON.parse(tilesetMetadata.carto_quadkey_zoom);
+            _partitioner = initializePartitioner(params);
+        }
+
+        const parentQuadkeys = getParentQuadkeysFromTiles(tiles);
         const parentQuadkeysFilter = parentQuadkeys.length ? `carto_quadkey IN (${parentQuadkeys})` : 'TRUE';
         const tilesFilter = tiles.map((tile) => tileFilter(tile)).join(' OR ');
         const sqlQuery = `
@@ -19461,8 +19746,8 @@ class BigQueryTilesetClient {
                     const x = parseInt(row.f[1].v);
                     const y = parseInt(row.f[2].v);
                     const mvt = tilesetMetadata.compression === 'gzip'
-                        ? Object(pako__WEBPACK_IMPORTED_MODULE_2__["inflate"])(atob(row.f[3].v))
-                        : Object(base64_arraybuffer__WEBPACK_IMPORTED_MODULE_1__["decode"])(row.f[3].v);
+                        ? Object(pako__WEBPACK_IMPORTED_MODULE_3__["inflate"])(atob(row.f[3].v))
+                        : Object(base64_arraybuffer__WEBPACK_IMPORTED_MODULE_2__["decode"])(row.f[3].v);
                     mvts.push({ z, x, y, buffer: mvt });
                     missedTiles = missedTiles.filter((t) => !(t.z === z && t.x === x && t.y === y));
                 }
@@ -19492,49 +19777,30 @@ class BigQueryTilesetClient {
     }
 }
 
-function getParentIntegerQuadkeysFromTiles (tiles, zOutput) {
-    if (zOutput === 0) {
-        return [];
+function initializePartitioner (parameters) {
+    if (parameters.version !== 1) {
+        throw new Error('Unknown quadkey version');
     }
+    const zRange = {
+        zmin: parameters.zmin,
+        zmax: parameters.zmax
+    };
+    const zmaxBBox = {
+        xmin: parameters.xmin,
+        xmax: parameters.xmax,
+        ymin: parameters.ymin,
+        ymax: parameters.ymax
+    };
+    const partitions = parameters.partitions;
+    return new _BQQuadkey__WEBPACK_IMPORTED_MODULE_1__["default"](partitions, zRange, zmaxBBox);
+}
 
+function getParentQuadkeysFromTiles (tiles) {
     let result = new Set();
     for (let tile of tiles) {
-        result.add(getParentIntegerQuadkeyFromTile(tile, zOutput));
+        result.add(_partitioner.getPartition({ z: tile.z, x: tile.x, y: tile.y }));
     }
-
     return [...result].filter(x => x !== null);
-}
-
-function getParentIntegerQuadkeyFromTile (tile, zOutput) {
-    if (tile.z === 0) {
-        return null;
-    }
-
-    zOutput = (zOutput === undefined || zOutput === null || zOutput < 0) ? tile.z : zOutput;
-
-    let index = getQuadkeyFromTile(tile);
-
-    if (index.length >= zOutput) {
-        index = index.substring(0, zOutput);
-    } else {
-        index += '0'.repeat(zOutput - index.length);
-    }
-
-    return parseInt(index, 4);
-}
-
-function getQuadkeyFromTile (tile) {
-    let index = '';
-
-    for (let z0 = tile.z; z0 > 0; z0--) {
-        let b = 0;
-        let mask = 1 << (z0 - 1);
-        if ((tile.x & mask) !== 0) b++;
-        if ((tile.y & mask) !== 0) b += 2;
-        index += b.toString();
-    }
-
-    return index;
 }
 
 function tileFilter (tile) {
@@ -21077,4 +21343,4 @@ function isTimeRange (t) {
 /***/ })
 
 /******/ });
-//# sourceMappingURL=carto-vl-cf2b1738bce7a41a17b6.worker.js.map
+//# sourceMappingURL=carto-vl-798e4cb504254db0a1cb.worker.js.map
